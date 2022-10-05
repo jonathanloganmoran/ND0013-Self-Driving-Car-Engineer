36edc9973e432ae58b64203cbbcff229046086d9
jonathanloganmoran <jonathan.moran107@gmail.com>
2022-10-05 13:10:35 -0700

    :hammer: Cleaning up preprocessing functions

--- 1-Computer-Vision/1-1-Object-Detection-in-Urban-Environments/scripts/preprocessing/utils.py
+++ 1-Computer-Vision/1-1-Object-Detection-in-Urban-Environments/scripts/preprocessing/utils.py
@@ -1,11 +1,15 @@
 import logging
-
-import tensorflow.compat.v1 as tf
+from object_detection.builders.dataset_builder import build as build_dataset
 from object_detection.inputs import train_input
 from object_detection.protos import input_reader_pb2
-from object_detection.builders.dataset_builder import build as build_dataset
 from object_detection.utils.config_util import get_configs_from_pipeline_file
+import tensorflow as tf
+from typing import List, Tuple, TypeVar
 from waymo_open_dataset import dataset_pb2 as open_dataset
+
+
+### Creating a not-so-long custom variable types for typing hints
+RepeatedCompositeContainer = TypeVar(google.protobuf.pyext._message.RepeatedCompositeContainer)
 
 
 def get_dataset(tfrecord_path, label_map='label_map.pbtxt'):
@@ -25,82 +29,86 @@ def get_dataset(tfrecord_path, label_map='label_map.pbtxt'):
     return dataset
 
 
-def get_module_logger(mod_name):
-    """ simple logger """
+def get_module_logger(mod_name: str) -> logging.Logger:
+    """Initialises a console logger instance.
+    
+    :param mod_name: the model name to assign to the logger.
+    :returns: a logger instance.
+    """
+    
+    ### Setting up the console logger and formatter
     logger = logging.getLogger(mod_name)
     handler = logging.StreamHandler()
     formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s')
     handler.setFormatter(formatter)
     logger.addHandler(handler)
     logger.setLevel(logging.DEBUG)
+    ### Prevent messages going to root handler
+    logger.propagate = False
     return logger
 
 
 def get_train_input(config_path):
-  """
-  Get the tf dataset that inputs training batches
-  args:
-    - config_path [str]: path to the edited config file
-  returns:
-    - dataset [tf.Dataset]: data outputting augmented batches
-  """
-  # parse config
-  configs = get_configs_from_pipeline_file(config_path)
-  train_config = configs['train_config']
-  train_input_config = configs['train_input_config']
-
-  # get the dataset
-  dataset = train_input(train_config, train_input_config, configs['model'])
-  return dataset
-
-def parse_frame(frame, camera_name='FRONT'):
-    """ 
-    take a frame, output the bboxes and the image
-
-    dataset = tf.data.TFRecordDataset(FILENAME, compression_type='')
-      for data in dataset:
-      frame = open_dataset.Frame()
-      frame.ParseFromString(bytearray(data.numpy()))
-    
-    args:
-      - frame [waymo_open_dataset.dataset_pb2.Frame]: a waymo frame, contains images and annotations
-      - camera_name [str]: one frame contains images and annotations for multiple cameras
-    
-    returns:
-      - encoded_jpeg [bytes]: jpeg encoded image
-      - annotations [protobuf object]: bboxes and classes
     """
-    # get image
+    Get the tf dataset that inputs training batches
+    args:
+    - config_path [str]: path to the edited config file
+    returns:
+    - dataset [tf.Dataset]: data outputting augmented batches
+    """
+    # parse config
+    configs = get_configs_from_pipeline_file(config_path)
+    train_config = configs['train_config']
+    train_input_config = configs['train_input_config']
+
+    # get the dataset
+    dataset = train_input(train_config, train_input_config, configs['model'])
+    return dataset
+
+
+def parse_frame(
+    frame: waymo_open_dataset.dataset_pb2.Frame, 
+    camera_name='FRONT'
+) -> Tuple[bytes, RepeatedCompositeContainer]:
+    """Returns the image and associated bounding box annotations from the frame.
+
+    :param frame: a frame from the Waymo Open Dataset, contains images and annotations.
+    :camera_name: str (optional), the camera whose image and annotations to obtain.
+    :returns: tuple, the encoded image and associated bounding box data.
+    """
+
     images = frame.images
     for im in images:
         if open_dataset.CameraName.Name.Name(im.name) != camera_name:
             continue
-        encoded_jpeg = im.image
-    
-    # get bboxes
+        else:
+            # Choosing only the image from the requested camera
+            encoded_jpeg = im.image
     labels = frame.camera_labels
     for lab in labels:
         if open_dataset.CameraName.Name.Name(lab.name) != camera_name:
             continue
-        annotations = lab.labels
+        else:
+            # Choosing only the annotations from the requested camera
+            annotations = lab.labels
     return encoded_jpeg, annotations
 
 
 def int64_feature(value):
-  return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 
 
 def int64_list_feature(value):
-  return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
 
 
 def bytes_feature(value):
-  return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
 
 def bytes_list_feature(value):
-  return tf.train.Feature(bytes_list=tf.train.BytesList(value=value))
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=value))
 
 
 def float_list_feature(value):
-  return tf.train.Feature(float_list=tf.train.FloatList(value=value))
+    return tf.train.Feature(float_list=tf.train.FloatList(value=value))
