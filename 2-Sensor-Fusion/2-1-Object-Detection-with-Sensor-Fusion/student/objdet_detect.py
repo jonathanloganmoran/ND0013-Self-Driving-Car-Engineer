c1bc87bf92887995f28a881cd97f1d098082d820
jonathanloganmoran <jonathan.moran107@gmail.com>
2022-11-04 12:56:50 -0700

    :sparkles: Adding SFA3D configs in `load_configs_model`

--- 2-Sensor-Fusion/2-1-Object-Detection-with-Sensor-Fusion/student/objdet_detect.py
+++ 2-Sensor-Fusion/2-1-Object-Detection-with-Sensor-Fusion/student/objdet_detect.py
@@ -1,4 +1,4 @@
-# -----------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # Project "3D Object Detection with LiDAR Data"
 # Copyright (C) 2020, Dr. Antje Muntzinger / Dr. Andreas Haja.
 #
@@ -9,8 +9,11 @@
 # You should have received a copy of the Udacity license with this program.
 #
 # https://www.udacity.com/course/self-driving-car-engineer-nanodegree--nd013
-# -----------------------------------------------------------------------------
-
+#
+# NOTE: The current version of this programme relies on Numpy to perform data 
+#       manipulation, however, a platform-specific implementation, e.g.,
+#       TensorFlow `tf.Tensor` data ops, is recommended.
+# ------------------------------------------------------------------------------
 
 ### General package imports
 import numpy as np
@@ -26,30 +29,50 @@ SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.
 sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 
 ### Model-related imports
+# The DarkNet model (Complex YOLO)
 from tools.objdet_models.darknet.models.darknet2pytorch import Darknet as darknet
 from tools.objdet_models.darknet.utils.evaluation_utils import post_processing_v2
+# The FPN ResNet model (SFA3D)
 from tools.objdet_models.resnet.models import fpn_resnet
-from tools.objdet_models.resnet.utils.evaluation_utils import decode, post_processing
+from tools.objdet_models.resnet.utils.evaluation_utils import decode
+from tools.objdet_models.resnet.utils.evaluation_utils import post_processing
+from tools.objdet_models.resnet.utils.torch_utils import _sigmoid
 
 
-# load model-related parameters into an edict
+### Load model-related parameters into an `edict` (ID_S3_EX1-3)
 def load_configs_model(model_name='darknet', configs=None):
+    """Loads the model configurations into an `edict` instance.
 
-    # init config file, if none has been passed
-    if configs==None:
+    :param model_name: the desired pre-trained model to load,
+        can be one of: ['darknet', 'fpn_resnet'].
+    :param configs: the EasyDict instance to update.
+    :returns: configs, the updated `edict` instance containing the
+        model parameters to use.
+    """
+
+    ### Step 0 : Initialising the path and `edict` variables
+    # Instantiate a new config dict if none has been provided
+    if not configs:
         configs = edict()  
-
-    # get parent directory of this file to enable relative paths
+    # Set the parent directory of this file to enable relative paths
     curr_path = os.path.dirname(os.path.realpath(__file__))
-    parent_path = configs.model_path = os.path.abspath(os.path.join(curr_path, os.pardir))    
-    
-    # set parameters according to model type
+    parent_path = configs.model_path = os.path.abspath(
+        os.path.join(curr_path, os.pardir)
+    )    
+    ### Step 1 : Update parameters according to model type
+    # If using the DarkNet model, update with relevant parameters
     if model_name == "darknet":
-        configs.model_path = os.path.join(parent_path, 'tools', 'objdet_models', 'darknet')
-        configs.pretrained_filename = os.path.join(configs.model_path, 'pretrained', 'complex_yolov4_mse_loss.pth')
+        configs.model_path = os.path.join(
+            parent_path, 'tools', 'objdet_models', 'darknet'
+        )
+        configs.pretrained_filename = os.path.join(
+            configs.model_path, 'pretrained', 'complex_yolov4_mse_loss.pth'
+        )
         configs.arch = 'darknet'
         configs.batch_size = 4
-        configs.cfgfile = os.path.join(configs.model_path, 'config', 'complex_yolov4.cfg')
+        configs.cfgfile = os.path.join(
+            configs.model_path, 'config', 'complex_yolov4.cfg'
+        )
         configs.conf_thresh = 0.5
         configs.distributed = False
         configs.img_size = 608
@@ -58,23 +81,100 @@ def load_configs_model(model_name='darknet', configs=None):
         configs.num_workers = 4
         configs.pin_memory = True
         configs.use_giou_loss = False
-
+    # If using the FPN ResNet / SFA3D model, update with relevant parameters
     elif model_name == 'fpn_resnet':
         ####### ID_S3_EX1-3 START #######     
-        #######
         print("student task ID_S3_EX1-3")
-
-        #######
+        # Path to the pre-trained model weights
+        configs.model_path = os.path.join(
+            parent_path, 'tools', 'objdet_models', 'resnet'
+        )
+        configs.pretrained_filename = os.path.join(
+            configs.model_path,'pretrained', 'fpn_resnet_18_epoch_300.pth'
+        )
+        ### Step 2 : Adding and updating configs defined in `sfa/test.py`
+        # The folder name to use for saving logs, trained models, etc.
+        configs.saved_fn = 'fpn_resnet'    # Equivalent to 'fpn_resnet_18'
+        # The name of the model architecture
+        configs.arch = 'fpn_resnet'
+        # The path to the pre-trained model
+        configs.pretrained_path = pretrained_filename
+        # The number of top 'K'
+        configs.K = 50
+        # If True, cuda is not used
+        configs.no_cuda = False
+        # GPU index to use
+        configs.gpu_idx = 0
+        # Subset of dataset to run and debug
+        configs.num_samples = None
+        # Number of threads for loading data 
+        configs.num_workers = 1
+        # Number of samples per mini-batch
+        configs.batch_size = 1
+        # 
+        configs.peak_thresh = 0.2
+        # If True, output image of testing phase will be saved
+        configs.save_test_output = False
+        # Type of test output (can be one of: ['image', 'video'])
+        configs.output_format = 'image'
+        # The video filename to use (if the output format is 'video')
+        configs.output_video_fn = 'out_fpn_resnet'
+        # The width of the output
+        configs.output_width = 608
+        configs.pin_memory = True
+        # Is False when testing on a single GPU only
+        configs.distributed = False
+        # The input Bird's-Eye View (BEV) image size
+        configs.input_size = (608, 608)
+        # The bounding box anchor size for balanced L1 loss
+        configs.hm_size = (152, 152)
+        # The down-sampling ratio S s.t. (H/S, W/S, C)
+        # for C num. classes and heatmap for main center of size (H, W)
+        configs.down_ratio = 4
+        # Max. number of predictions to keep, i.e., the maximum number
+        # of predictions whose center confidences are greater than 0.2
+        configs.max_objects = 50
+        configs.imagenet_pretrained = False
+        # The num. channels in the convolutional layer for the output head
+        # 64 for ResNet and 256 for DLA
+        configs.head_conv = 64
+        configs.num_classes = 3
+        # The center offset, i.e., (H/S, W/S, 2)
+        configs.num_center_offset = 2
+        # The z-coordinate dimension, i.e., (H/S, W/S, 1)
+        configs.num_z = 1
+        # The number of channels in the input, i.e., (H, W, 3)
+        configs.num_dim = 3
+        # Model estimates the complex-valued yaw angles
+        # The `im` and `re` fractions are directly regressed using `l1_loss`
+        configs.num_direction = 2    # sin, cos
+        # Parameters used for the regression tasks, i.e., the
+        # Sigmoid / Focal / L1 / Balanced L1 loss functions
+        configs.heads = {
+            'hm_cen': configs.num_classes,
+            'cen_offset': configs.num_center_offset,
+            'direction': configs.num_direction,
+            'z_coor': configs.num_z,
+            'dim': configs.num_dim
+        }
+        configs.num_input_features = 4
+        configs.root_dir = '../'
+        configs.dataset_dir = os.path.join(
+            configs.root_dir, 'dataset'
+        )
+        if configs.save_test_output:
+            configs.result_dir = os.path.join(
+                configs.root_dir, 'results', configs.saved_fn
+            )
         ####### ID_S3_EX1-3 END #######     
-
     else:
-        raise ValueError("Error: Invalid model name")
-
-    # GPU vs. CPU
+        raise ValueError("Error: Invalid model name '{model_name}'")
+    ### Configurations for training on either GPU vs. CPU
     configs.no_cuda = True # if true, cuda is not used
     configs.gpu_idx = 0  # GPU index to use.
-    configs.device = torch.device('cpu' if configs.no_cuda else 'cuda:{}'.format(configs.gpu_idx))
-
+    configs.device = torch.device(
+        'cpu' if configs.no_cuda else 'cuda:{}'.format(configs.gpu_idx)
+    )
     return configs
 
 
