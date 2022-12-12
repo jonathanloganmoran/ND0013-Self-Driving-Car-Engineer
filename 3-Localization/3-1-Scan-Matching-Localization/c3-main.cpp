92cbf21e99c65e6872d83f03c6e64b555d0e4f90
jonathanloganmoran <jonathan.moran107@gmail.com>
2022-12-12 15:11:04 -0800

    :sparkles: Finishing `main` — ICP localisation

--- 3-Localization/3-1-Scan-Matching-Localization/c3-main.cpp
+++ 3-Localization/3-1-Scan-Matching-Localization/c3-main.cpp
@@ -38,6 +38,9 @@
 
 
 /*** Defining the programme parameters ***/
+// Define the scan matching / registration algorithm to use
+// If `USE_ICP` is set to `false`, then the NDT algorithm will be used
+const static bool USE_ICP = true;
 // Set the base path relative to CWD where '.pcd' files are stored / saved
 const static std::string kBasePathRel = "../";
 // Minimum distance of the LiDAR scan points to preserve (in metres).
@@ -51,14 +54,64 @@ const static double kMinimumDistanceLidarDetections = 8.0;
 // will be ignored.
 const static int kMaximumScanPointsMap = 5000;
 
+/*** Defining the ICP hyperparameters ***/
+// The maximum correspondence distance between `source` and `target`
+// i.e., correspondences with higher distances will be ignored
+// Should be sufficiently large s.t. all points are considered.
+// Rule of thumb: set to max distance between two points in the point clouds.
+const static double kMaxCorrespondenceDistanceICP = 5;  		// Metres (m)
+// The maximum number of ICP iterations to perform before termination.
+// Should be large enough to ensure the algorithm has sufficient time to
+// converge. Rule of thumb: set to twice the number of points in the PCL.
+const static int kMaximumIterationsICP = 120;
+// The maximum epsilon threshold between previous transformation and current
+// estimated transformation. Rule of thumb: set between 1e-4 and 1e-8.
+const static double kTransformationEpsilonICP = 1e-4;
+// The maximum sum of Euclidean squared errors between two consecutive steps
+// before algorithm is considered to be converged.
+// Rule of thumb: set between 1 and 10.
+const static double kEuclideanFitnessEpsilonICP = 2;
+// The inlier distance threshold for the internal RANSAC outlier rejection loop
+// Note: a point is considered an inlier if the distance between `target` and
+// transformed `source` is smaller than inlier distance threshold.
+// Default: 0.05m, Rule of thumb: set between 0.2 and 0.3 m.
+const static double kRANSACOutlierRejectionThresholdICP = 0.2;  // Metres (m)
+
+/*** Defining the NDT hyperparameters ***/
+// The maximum number of NDT iterations to perform before termination.
+// Each iteration the NDT algorithm attempts to improve the accuracy of the
+// transformation. Default: 150, Rule of thumb: start with default and
+// decrease or increase depending on size and complexity of data set.
+const static int kMaximumIterationsNDT = 150;
+// The step size taken for each iteration of the NDT algorithm.
+// Used in the More-Thuente line search to determine how much the
+// transformation matrix is updated at each iteration. A larger step size
+//  will lead to faster convergence, but may lead to inaccurate results.
+// Default: 0.1, Rule of thumb: decrease if NDT is coverging too quickly.
+const static double kStepSizeNDT = 1.0;
+// The transformation epsilon threshold for the NDT algorithm.
+// The maximum epsilon threshold between the previous and current estimated
+// transformation. Rule of thumb: set between 1e-4 and 1e-8.
+const static double kTransformationEpsilonNDT = 1e-3;
+// The resolution of the NDT `VoxelGridCovariance`
+// i.e., the resolution side length of the 3D voxel to use for discretisation
+// in the NDT algorithm. Here we assume a cubioid, i.e., each of the sides
+// (`lx`, `ly`, `lz`) have the same dimensions according to what is set here.
+const static double kVoxelGridCovarianceNDT = 1.0;
+
+/*** Setting voxel grid hyperparameters ***/
+// Resolution of each 3D voxel ('box') used to downsample the point cloud
+// Here we assume a cuboid, i.e., each of the sides (`lx`, `ly`, `lz`) have
+// the same dimensions according to what is set here (in metres).
+const static double kLeafSizeVoxelGrid = 0.5;
+
+
 /*** Setting the intial state variables ***/
 PointCloudT pclCloud;
 carla::client::Vehicle::Control control;
 std::chrono::time_point<std::chrono::system_clock> currentTime;
 std::vector<ControlState> controlStateHistory;
 bool refreshView = false;
-
-
 
 
 /* Event handler that updates the PCL Viewer and vehicle state.
@@ -253,6 +306,7 @@ void Actuate(
  * @param  vehicle	 CARLA `Vehicle` instance generating a new transform.
  * @param  truePose  Ground-truth pose to set with info from CARLA. 
  * @param  poseRef	 Last known 3D pose of the `vehicle`.
+ * @param  control	 Current control state of the vehicle to update.
  * @param  controlStateHistory  Vector of previous `ControlState` instances.
  * */
 void UpdatePoseAndControl(
@@ -260,6 +314,7 @@ void UpdatePoseAndControl(
         auto& vehicle,
         Pose3D& truePose,
         Pose3D& poseRef,
+		auto& control,
         std::vector<ControlState>& controlStateHistory
 ) {
 	// Update the PCL Viewer with the latest vehicle pose and control state
@@ -325,7 +380,7 @@ void UpdatePoseError(
 	pcl::visualization::PCLVisualizer::Ptr& viewer,
 	Pose3D& pose,
 	Pose3D& truePose,
-	double maxError
+	double& maxError
 	
 ) {
 	double poseError = sqrt(
@@ -373,6 +428,160 @@ void UpdatePoseError(
 }
 
 
+/* Implements the Iterative Closest Point (ICP) algorithm.
+ *
+ * Returns the estimated transformation (i.e., rotation and translation)
+ * between the `source` and `target` point clouds. The transformation
+ * parameters are iteratively computed using the Point Cloud Library (PCL)
+ * ICP implementation.
+ *
+ * Note: the `source` point cloud is first transformed into the coordinate
+ * frame of the given `startingPose` before it is registered to the `target`.
+ *
+ * @param    target			Reference point cloud to align the `source` to.
+ * @param    source			Starting point cloud to align to the `target`.
+ * @param    startingPose   3D pose to transform the `source` point cloud by.
+ * param     iterations		Maximum number of iterations to run ICP for.
+ * @returns  transformationMatrix
+ */
+Eigen::Matrix4d ICP(
+        PointCloudT::Ptr target,
+        PointCloudT::Ptr source,
+        Pose3D startingPose,
+        int iterations
+) {
+    // Initialising the output as the 4x4 identity matrix
+      Eigen::Matrix4d transformationMatrix = Eigen::Matrix4d::Identity();
+      // Construct the 3D transformation matrix from the `startingPose`
+    Eigen::Matrix4d startingPoseTransform = transform3D(
+        startingPose.rotation.yaw,
+        startingPose.rotation.pitch,
+        startingPose.rotation.roll,
+        startingPose.position.x,
+        startingPose.position.y,
+        startingPose.position.z
+    );
+    // Transform the `source` point cloud by the `startingPose`
+    PointCloudT::Ptr sourceTransformed(new PointCloudT);
+    pcl::transformPointCloud(
+        *source,
+        *sourceTransformed,
+        startingPoseTransform
+    );
+    /*** Compute the scan matching registration with the ICP algorithm ***/
+    // Start a `TicToc` time tracking instance to profile the ICP algorithm
+    pcl::console::TicToc time;
+    time.tic();
+    // Instantiate the ICP class
+    pcl::IterativeClosestPoint<PointT, PointT> icp;
+    // Set the input `source` and `target` point clouds
+    icp.setInputSource(sourceTransformed);
+    icp.setInputTarget(target);
+    // Set the ICP hyperparameters
+    icp.setMaxCorrespondenceDistance(kMaxCorrespondenceDistanceICP);
+    icp.setMaximumIterations(iterations);
+    icp.setTransformationEpsilon(kTransformationEpsilonICP);
+    icp.setEuclideanFitnessEpsilon(kEuclideanFitnessEpsilonICP);
+    icp.setRANSACOutlierRejectionThreshold(kRANSACOutlierRejectionThresholdICP);
+    // Perform the registration (alignment) with ICP
+    PointCloudT::Ptr outputICP(new PointCloudT);
+    icp.align(*outputICP);
+    std::cout << "Finished ICP alignment in " << time.toc() << " ms" << "\n";
+    std::cout << "ICP converged: " << std::boolalpha << icp.hasConverged();
+    std::cout << ", Fitness score: " << icp.getFitnessScore() << "\n";
+    // Check if ICP algorithm converged
+    if (icp.hasConverged()) {
+        // Get estimated transformation matrix
+        transformationMatrix = icp.getFinalTransformation().cast<double>();
+        // Transform the estimated matrix into `startingPose` coordinate frame
+        transformationMatrix *= startingPoseTransform;
+        return transformationMatrix;
+    }
+    else {
+        std::cout << "WARNING: ICP did not converge" << "\n";
+        // Return the identity matrix (i.e., apply no transformation)
+        return transformationMatrix;
+    }
+}
+
+
+/* Implements the Normal Distributions Transform (NDT) algorithm.
+ *
+ * The 3D Normal Distributions Transform (NDT) in Point Cloud Library (PCL)
+ * implements the Magnussen, M. et al., 2009 paper titled
+ * <b> The Three-Dimensional Normal-Distributions Transform — an Efficient
+ * Representation for Registration, Surface Analysis, and Loop Detection.
+ * PhD thesis, Örebro Studies in Technology, 36(1):1-201. Örebro universitet.
+ * 2009. </b>
+ *
+ * Returns the estimated transformation (i.e., rotation and translation)
+ * between the `source` and `target` point clouds. The transformation
+ * parameters are iteratively computed using the Point Cloud Library (PCL)
+ * implementation of the NDT algorithm.
+ *
+ * Note: the `source` point cloud is first transformed into the coordinate
+ * frame of the given `startingPose` before it is registered to the `target`.
+ *
+ * @param  target		 Reference point cloud to align the `source` to.
+ * @param  source		 Starting point cloud to align to the `target`.
+ * @param  startingPose  3D pose to transform the `source` point cloud by.
+ * @param  iterations	 Maximum number of iterations to run NDT for.
+ * @returns transformationMatrix
+ */
+Eigen::Matrix4d NDT(
+        PointCloudT::Ptr target,
+        PointCloudT::Ptr source,
+        Pose3D startingPose,
+        int iterations
+) {
+    // Initialising the output as the 4x4 identity matrix
+    Eigen::Matrix4d transformationMatrix = Eigen::Matrix4d::Identity();
+    // Construct the initial 3D transformation matrix estimate
+    Eigen::Matrix4f initialGuess = transform3D(
+        startingPose.rotation.yaw,
+        startingPose.rotation.pitch,
+        startingPose.rotation.roll,
+        startingPose.position.x,
+        startingPose.position.y,
+        startingPose.position.z
+    ).cast<float>();
+    /*** Compute the scan matching registration with the NDT algorithm ***/
+    pcl::console::TicToc time;
+    time.tic();
+	// Initialise the NDT instance
+	pcl::NormalDistributionsTransform<pcl::PointXYZ, pcl::PointXYZ> ndt;
+	// Setting the NDT hyperparameters
+    ndt.setMaximumIterations(iterations);
+	ndt.setStepSize(kStepSizeNDT);
+	ndt.setTransformationEpsilon(kTransformationEpsilonNDT);
+	ndt.setResolution(kVoxelGridCovarianceNDT);
+    // Set the input `source` point cloud
+    ndt.setInputSource(source);
+	// Set the input `target` point cloud
+	ndt.setInputTarget(target);
+    // Perform the registration (alignment) with NDT
+    pcl::PointCloud<pcl::PointXYZ>::Ptr outputNDT(
+        new pcl::PointCloud<pcl::PointXYZ>
+    );
+    ndt.align(*outputNDT, initialGuess);
+    std::cout << "Finished NDT alignment in " << time.toc() << " ms" << "\n";
+    std::cout << "NDT converged: " << std::boolalpha << ndt.hasConverged();
+    std::cout << ", Fitness score: " << ndt.getFitnessScore() << "\n";
+    // Check if NDT algorithm has converged
+    if (ndt.hasConverged()) {
+        // Get the estimated transformation matrix
+        transformationMatrix = ndt.getFinalTransformation().cast<double>();
+        // Return estimated transformation matrix
+        return transformationMatrix;
+    }
+      else {
+        std::cout << "WARNING: NDT did not converge" << "\n";
+        // Return the identity matrix (i.e., apply no transformation)
+        return transformationMatrix;
+    }
+}
+
+
 /*
  *
  */
@@ -382,7 +591,7 @@ int main() {
         "localhost",
         2000
     );
-    client.SetTimeout(std::chrono::seconds(2));		// Seconds (s)
+    client.SetTimeout(std::chrono::seconds(2));
 	/*** Setting actors and blueprint ***/
 	// Creating and spawning a vehicle actor and map from the blueprint
 	// Load an empty world
@@ -392,11 +601,11 @@ int main() {
 	auto vehicles = blueprintLibrary->Filter("vehicle");
 	// Set the spawn point
 	auto map = world.GetMap();
-	auto transform = map->GetRecommendedSpawnPoints()[1];
+	auto spawnPointTransform = map->GetRecommendedSpawnPoints()[1];
 	// Spawn the ego-vehicle at the spawn point
 	auto egoActor = world.SpawnActor(
 		(*vehicles)[12],
-		transform
+		spawnPointTransform
 	);
 	/*** Creating and configuring a simulated LiDAR sensor ***/
     // Create a new simulated LiDAR sensor
@@ -419,6 +628,8 @@ int main() {
     auto lidar = boost::static_pointer_cast<
         carla::client::Sensor
     >(lidarActor);
+    // Set the scan matching algorithm state to 'initialise'
+    bool initScan = true;
 	// Initialise the state variable to accept incoming LiDAR scans
     bool fetchNewScan = true;
 	// Initialise new time tracking instances 
@@ -476,7 +687,7 @@ int main() {
 			auto scan = boost::static_pointer_cast<
 				carla::sensor::data::LidarMeasurement
 			>(data);
-			for (auto& detection : *scan) {
+			for (auto detection : *scan) {
 				if (!InEgoVehicleRange(detection)) { 
 					// Don't include points touching the ego-vehicle
 					pclCloud.points.push_back(
@@ -541,22 +752,57 @@ int main() {
 			vehicle,
 			truePose,
 			poseRef,
+			control,
 			controlStateHistory
 		);
   		viewer->spinOnce();
 		// Compute the pose transformation using the scan matching algorithm
 		if (!fetchNewScan) {
-			// Done processing latest scan, set flag to fetch new scan 
+			if (initScan == true) {
+				// Use the ground-truth vehicle pose to initialise the estimate 
+				pose.position = truePose.position;
+				pose.rotation = truePose.rotation;
+			}
+			// Done processing latest scan, set flag to fetch new scan
 			fetchNewScan = true;
 			// TODO: (Filter scan using voxel filter)
-			// TODO: Find pose transform by using ICP or NDT matching
-			//pose = ....
+			pcl::VoxelGrid<PointT> voxelGrid;
+			voxelGrid.setInputCloud(scanCloud);
+			voxelGrid.setLeafSize(
+				kLeafSizeVoxelGrid,
+				kLeafSizeVoxelGrid,
+				kLeafSizeVoxelGrid
+			);
+			typename pcl::PointCloud<PointT>::Ptr cloudFiltered(
+				new pcl::PointCloud<PointT>
+			);
+			voxelGrid.filter(*cloudFiltered);
+			// Compute the pose transform using ICP or NDT scan matching
+			Eigen::Matrix4d transformEstimate = USE_ICP ? ICP(
+				mapCloud,
+				cloudFiltered,
+				pose,
+				kMaximumIterationsICP
+			) : NDT(
+				mapCloud,
+				cloudFiltered,
+				pose,
+				kMaximumIterationsNDT
+			);
+			// Get the pose associated with this transformation estimate
+			pose = getPose3D::getPose(transformEstimate);
 			// TODO: Transform scan so it aligns with ego's actual pose and render that scan
+			PointCloudT::Ptr correctedScan(new PointCloudT);
+			pcl::transformPointCloud(
+				*cloudFiltered,
+				*correctedScan,
+				transformEstimate
+			);
 			viewer->removePointCloud("scan");
 			// TODO: Change `scanCloud` below to your transformed scan
 			renderPointCloud(
 				viewer, 
-				scanCloud, 
+				correctedScan,
 				"scan", 
 				Color(1, 0, 0)
 			);
@@ -574,6 +820,7 @@ int main() {
 				truePose,
 				maxError
 			);
+			pclCloud.points.clear();
   		}
 	}
 	return 0;
