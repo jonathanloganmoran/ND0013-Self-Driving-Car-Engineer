3030641796fdb3504b586e2cbab996d07f35221c
jonathanloganmoran <jonathan.moran107@gmail.com>
2022-12-16 14:32:52 -0800

    :alembic::white_check_mark: Passing sanity check — behaviour planner runs successfully

--- 4-Planning/Exercises/4-1-Behavior-Planning/1_tests.cc
+++ 4-Planning/Exercises/4-1-Behavior-Planning/1_tests.cc
@@ -103,41 +103,38 @@ void test_inefficiency_cost() {
 void test_behaviour_planner() {
   // impacts default behavior for most states
   int SPEED_LIMIT = 10;
-
   // all traffic in lane (besides ego) follow these speeds
-  vector<int> LANE_SPEEDS = {6,7,8,9}; 
-
+  std::vector<int> LANE_SPEEDS = {6, 7, 8, 9}; 
   // Number of available "cells" which should have traffic
   double TRAFFIC_DENSITY = 0.15;
-
   // At each timestep, ego can set acceleration to value between 
   //   -MAX_ACCEL and MAX_ACCEL
   int MAX_ACCEL = 2;
-
   // s value and lane number of goal.
-  vector<int> GOAL = {300, 0};
-
+  std::vector<int> GOAL = {300, 0};
   // These affect the visualization
   int FRAMES_PER_SECOND = 4;
   int AMOUNT_OF_ROAD_VISIBLE = 40;
-
-  Road road = Road(SPEED_LIMIT, TRAFFIC_DENSITY, LANE_SPEEDS);
-
+  Road road = Road(SPEED_LIMIT, 
+                   TRAFFIC_DENSITY, 
+                   LANE_SPEEDS
+  );
   road.update_width = AMOUNT_OF_ROAD_VISIBLE;
-
   road.populate_traffic();
-
   int goal_s = GOAL[0];
   int goal_lane = GOAL[1];
-
-  // configuration data: speed limit, num_lanes, goal_s, goal_lane, 
-  //   and max_acceleration
+  // Configuration data: 
+  // {speed limit, num_lanes, goal_s, goal_lane, max_acceleration}
   int num_lanes = LANE_SPEEDS.size();
-  vector<int> ego_config = {SPEED_LIMIT,num_lanes,goal_s,goal_lane,MAX_ACCEL};
-   
+  std::vector<int> ego_config = {
+      SPEED_LIMIT,
+      num_lanes,
+      goal_s,
+      goal_lane,
+      MAX_ACCEL
+  };
   road.add_ego(2,0, ego_config);
   int timestep = 0;
-  
   while (road.get_ego().s <= GOAL[0]) {
     ++timestep;
     if (timestep > 100) {
@@ -147,16 +144,15 @@ void test_behaviour_planner() {
     road.display(timestep);
     //time.sleep(float(1.0) / FRAMES_PER_SECOND);
   }
-
   Vehicle ego = road.get_ego();
   if (ego.lane == GOAL[1]) {
-    cout << "You got to the goal in " << timestep << " seconds!" << endl;
+    std::cout << "You got to the goal in " << timestep << " seconds!" << "\n";
     if(timestep > 35) {
-      cout << "But it took too long to reach the goal. Go faster!" << endl;
+      std::cout << "But it took too long to reach the goal. Go faster!" << "\n";
     }
   } else {
-    cout << "You missed the goal. You are in lane " << ego.lane 
-         << " instead of " << GOAL[1] << "." << endl;
+    std::cout << "You missed the goal. You are in lane " << ego.lane;
+    std::cout << " instead of " << GOAL[1] << "." << "\n";
   }
 }
 
