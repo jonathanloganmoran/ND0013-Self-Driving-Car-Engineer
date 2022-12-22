ebc26ab1690841664f0cd14c70a4fb3c901cefd4
jonathanloganmoran <jonathan.moran107@gmail.com>
2022-12-22 13:25:17 -0800

    :tada: Adding starter code for Exercise 4.2.2: Quintic Polynomial Solver in C++

---
+++ 4-Planning/Exercises/4-2-Trajectory-Generation/2_jerk_minimising_trajectory.cc
@@ -0,0 +1,29 @@
+
+
+#include "2_jerk_minimising_trajectory.h"
+
+/**
+ * TODO: complete this function
+ */
+vector<double> JMT(vector<double> &start, vector<double> &end, double T) {
+  /**
+   * Calculate the Jerk Minimizing Trajectory that connects the initial state
+   * to the final state in time T.
+   *
+   * @param start - the vehicles start location given as a length three array
+   *   corresponding to initial values of [s, s_dot, s_double_dot]
+   * @param end - the desired end state for vehicle. Like "start" this is a
+   *   length three array.
+   * @param T - The duration, in seconds, over which this maneuver should occur.
+   *
+   * @output an array of length 6, each value corresponding to a coefficent in 
+   *   the polynomial:
+   *   s(t) = a_0 + a_1 * t + a_2 * t**2 + a_3 * t**3 + a_4 * t**4 + a_5 * t**5
+   *
+   * EXAMPLE
+   *   > JMT([0, 10, 0], [10, 10, 0], 1)
+   *     [0.0, 10.0, 0.0, 0.0, 0.0, 0.0]
+   */
+  std::vector<double> coeffs;
+  return coeffs;
+}
---
+++ 4-Planning/Exercises/4-2-Trajectory-Generation/2_jerk_minimising_trajectory.h
@@ -0,0 +1,13 @@
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+#include "Dense"
+#include "grader.h"
+
+using std::vector;
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+
+vector<double> JMT(vector<double> &start, vector<double> &end, double T);
