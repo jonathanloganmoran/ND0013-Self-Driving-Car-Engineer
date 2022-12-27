2dd0035f94f7015d61e0c27efc770c2b04e3ccf0
jonathanloganmoran <jonathan.moran107@gmail.com>
2022-12-27 10:20:13 -0800

    :sparkles: Adding `generate_offset_goals` function

--- 4-Planning/Exercises/4-3-Motion-Planning/2_offset_goals.cc
+++ 4-Planning/Exercises/4-3-Motion-Planning/2_offset_goals.cc
@@ -1,153 +1,89 @@
-/**********************************************
- * Self-Driving Car Nano-degree - Udacity
- *  Created on: October 20, 2020
- *      Author: Munir Jojo-Verge
- **********************************************/
+/* ------------------------------------------------------------------------------
+ * Lesson "4.3: Motion Planning"
+ * Authors     : Munir Jojo-Verge.
+ *
+ * Modified by : Jonathan L. Moran (jonathan.moran107@gmail.com)
+ *
+ * Purpose of this file: Implements the goal-offset path planning functions.
+ *                       The new path for the vehicle is computed as a
+ *                       deviation of the orignal path w.r.t. centre lane-line.
+ * ----------------------------------------------------------------------------
+ */
 
-/*
-In this exercise you will generate "_num_goals" goals, offset from the
-center goal at a distance "_goal_offset". The offset goals will be aligned on a
-perpendicular line to the heading of the main goal. To get a perpendicular
-angle, just add 90 degrees (or π/2 rad) to the main goal heading (ϴ). After
-that you will just need to calculate the x and y coordinates for each offset
-goal using the equations presented in the lectures:
-X_offset = X_center_goal +
-goal_number * Offset_distance * cos(ϴ+π/2);
-Y_offset = Y_center_goal +
-goal_number * Offset_distance * sin(ϴ+π/2)
+#include "2_offset_goals.h"
 
-NOTE:
-1) goal_number will go from: -3, -2, -1, 0, 1 ,2 ,3  (for 7 total goals)
-2) When goal_number = 0, we will get the "center" goal.
-*/
 
-#include <math.h>
+/*** Global variables ***/
+// Distance from the centre lane line
+float _goal_offset = 1.0;
+// Number of offset paths to generate
+int _num_goals = 7;
 
-#include <cfloat>
-#include <cmath>
-#include <iostream>
-#include <vector>
-
-// In Meters
+/*** Struct definitions ***/
+// Stores the 3D position on the road surface
+// Each value is given in metres (m)
 struct Location {
   float x = 0.0;
   float y = 0.0;
   float z = 0.0;
 };
-
-// In Radians
+// Stores the 3D rotation of the path w.r.t. vehicle heading
+// Each value is given in radians
 struct Rotation {
   float pitch = 0.0;
   float yaw = 0.0;
   float roll = 0.0;
 };
-
+// Stores the path instance (i.e., deviation from the centre lane line)
 struct State {
   Location location;
   Rotation rotation;
 };
 
-float _goal_offset = 1.0;
-int _num_goals = 7;
 
-std::vector<State> generate_offset_goals(const State& goal_state) {
-  // Now we need to gernerate "_num_goals" goals offset from the center goal at
-  // a distance "_goal_offset".
+/* Generates the offset paths from the goal (centre lane line).
+ *
+ * The generated paths are aligned on a perpendicular line to the heading of
+ * the original goal state. The paths are offset from the centre lane line at
+ * a distance `_goal_offset`. The offset $x$- and $y$-coordinates are computed
+ * using a simple trigonometric relation, i.e.,
+ * $x_{offset} = x_{centre_goal} + goal_nr * offset_dist * cos(\phi + \pi / 2)$,
+ * $y_{offset} = y_{centre_goal} + goal_nr * offset_dist * cos(\phi + \pi / 2)$.
+ * 
+ * Here the `goal_nr` is defined as an element in the `_num_goals` vector,
+ * which are the positions on the left and right of the centre, i.e.,
+ * where `goal_nr = 0`. The `_num_goals` values to the left of the centre line
+ * are negative, wheras the values to the right of the centre line are positive.
+ *
+ * @param    goal_state     Starting 3D position and rotation of centre lane.
+ * @returns  goals_offset   3D positions / rotations deviated from centre.
+ */
+std::vector<State> generate_offset_goals(
+    const State& goal_state
+) {
+  // Create a vector of `_num_goals` path offsets,
+  // each are a deviation from the centre lane line (i.e., the goal state) 
   std::vector<State> goals_offset;
-
-  // The offset goals will be aligned on a perpendiclular line to the heading of
-  // the main goal. To get a perpendicular angle, just add 90 degrees (or PI/2
-  // rad) to the main goal heading.
-
-  // TODO-Perpendicular direction: ADD pi/2 to the goal yaw
-  // (goal_state.rotation.yaw)
-  // yaw_plus_90 = goal_state.rotation.yaw + pi/2;
-  auto yaw_plus_90 = ...;
-
-  for (int i = 0; i < _num_goals; ++i) {
+  // Get the yaw angle perpendicular to the goal state heading (centre line)
+  // from which to compute the path direction offsets from
+  // Note: pi / 2 = `M_PI_2`
+  float yaw_plus_90 = goal_state.rotation.yaw + M_PI_2;
+  // Compute the path offsets
+  for (int goal_nr = 0; goal_nr < _num_goals; ++goal_nr) {
     auto goal_offset = goal_state;
-    // This is the offset distance for goal "i"
-    float offset = (i - (int)(_num_goals / 2)) * _goal_offset;
-
-    // TODO-offset goal location: calculate the x and y coordinates of the
-    // offset goals using "offset" (calculated above) and knowing that the goals
-    // should be laid on a perpendicular line to the direction (yaw) of the main
-    // goal. You calculated this direction above (yaw_plus_90).
-    // HINT: use
-    // std::cos(yaw_plus_90) and std::sin(yaw_plus_90)
-    // X_offset = X_center_goal +
-    // goal_number * Offset_distance * cos(ϴ+π/2);
-    // Y_offset = Y_center_goal +
-    // goal_number * Offset_distance * sin(ϴ+π/2)
-    goal_offset.location.x = ...;
-    goal_offset.location.y = ...;
-
-    // std::cout << "x: " << goal_offset.location.x << std::endl;
-    // std::cout << "y: " << goal_offset.location.y << std::endl;
-    // std::cout << "yaw: " << goal_offset.rotation.yaw << std::endl;
-
+    // 1. Calculate the offset offset distance for goal "i"
+    float offset_product = (goal_nr - (int)(_num_goals / 2)) * _goal_offset;
+    // 2. Calculate the goal position along the $x$- and $y$-axes
+    // The offset path is defined perpendicular to the goal state (centre line),
+    // so we calculate the offset coordinates w.r.t. this perpendicular yaw direction
+    goal_offset.location.x += offset_product * std::cos(yaw_plus_90);
+    goal_offset.location.y += offset_product * std::sin(yaw_plus_90);
+    // UNCOMMENT TO PRINT THE COORDINATE AND YAW ANGLE OFFSET VALUES
+    // std::cout << "x: " << goal_offset.location.x << "\n";
+    // std::cout << "y: " << goal_offset.location.y << "\n";
+    // std::cout << "yaw: " << goal_offset.rotation.yaw << "\n";
     goals_offset.push_back(goal_offset);
   }
+  // Return the set of waypoints offset from the goal
   return goals_offset;
-}
-
-// ******* MAIN FUNCTION ********
-int main(int argc, const char* argv[]) {
-  State goal_state;
-  std::cout << "Test Case 1:" << std::endl;
-  goal_state.location.x = 0.0;
-  goal_state.location.y = 0.0;
-  goal_state.rotation.yaw = 0.0;
-  State offset_goal;
-  std::vector<State> expected;
-
-  offset_goal.location.x = -1.83697e-16;
-  offset_goal.location.y = -3;
-  offset_goal.rotation.yaw = 0;
-  expected.push_back(offset_goal);
-
-  offset_goal.location.x = -1.22465e-16;
-  offset_goal.location.y = -2;
-  offset_goal.rotation.yaw = 0;
-  expected.push_back(offset_goal);
-
-  offset_goal.location.x = -6.12323e-17;
-  offset_goal.location.y = -1;
-  offset_goal.rotation.yaw = 0;
-  expected.push_back(offset_goal);
-
-  offset_goal.location.x = 0;
-  offset_goal.location.y = 0;
-  offset_goal.rotation.yaw = 0;
-  expected.push_back(offset_goal);
-
-  offset_goal.location.x = 6.12323e-17;
-  offset_goal.location.y = 1;
-  offset_goal.rotation.yaw = 0;
-  expected.push_back(offset_goal);
-
-  offset_goal.location.x = 1.22465e-16;
-  offset_goal.location.y = 2;
-  offset_goal.rotation.yaw = 0;
-  expected.push_back(offset_goal);
-
-  offset_goal.location.x = 1.83697e-16;
-  offset_goal.location.y = 3;
-  offset_goal.rotation.yaw = 0;
-  expected.push_back(offset_goal);
-
-  auto result = generate_offset_goals(goal_state);
-  for (size_t i = 0; i < _num_goals; ++i) {
-    std::cout << (std::fabs(result[i].location.x - expected[i].location.x) <
-                              FLT_EPSILON &&
-                          std::fabs(result[i].location.y -
-                                    expected[i].location.y) < FLT_EPSILON &&
-                          std::fabs(result[i].rotation.yaw -
-                                    expected[i].rotation.yaw) < FLT_EPSILON
-                      ? "PASS"
-                      : "FAIL")
-              << std::endl;
-  }
-
-  return 0;
 }
