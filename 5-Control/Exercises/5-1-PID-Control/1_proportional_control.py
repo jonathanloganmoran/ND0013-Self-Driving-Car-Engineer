f7fcf79e8760536ab9324032098c180207d0cfb9
jonathanloganmoran <jonathan.moran107@gmail.com>
2023-01-03 17:23:30 -0800

    :art::bulb: Enforcing PEP 8 and adding descriptive docstrings / typing hints

--- 5-Control/Exercises/5-1-PID-Control/1_proportional_control.py
+++ 5-Control/Exercises/5-1-PID-Control/1_proportional_control.py
@@ -1,30 +1,64 @@
-# -----------
-# User Instructions
+# -----------------------------------------------------------------------------
+# Lesson "4.3: Motion Planning"
+# Authors     : Sebastian Thrun.
 #
-# Implement a P controller by running 100 iterations
-# of robot motion. The desired trajectory for the 
-# robot is the x-axis. The steering angle should be set
-# by the parameter tau so that:
+# Modified by : Jonathan L. Moran (jonathan.moran107@gmail.com)
 #
-# steering = -tau * crosstrack_error
+# Purpose of this file: Implements the Proportional Controller and simulates
+#                       robot motion along a horizontal reference trajectory.
 #
-# You'll only need to modify the `run` function at the bottom.
-# ------------
- 
-import random
-import numpy as np
-import matplotlib.pyplot as plt
+# NOTE: This programme is not functional in its current state (see TODO).
+# -----------------------------------------------------------------------------
 
-# ------------------------------------------------
-# 
-# this is the Robot class
-#
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+from typing import NoReturn
+
 
 class Robot(object):
-    def __init__(self, length=20.0):
+    '''The simulated Robot class.
+    
+    The robot moves about the 2D environment given a position (x, y) and
+    heading (`orientation`) angle, i.e., direction of motion.
+
+    The proportional controller used here follows the equation:
+        $\alpha = -\tau * \mathrm{CTE}$,
+    where the steering input angle $\alpha$ is computed w.r.t. the
+    cross-track error $\mathrm{CTE}$ proportional to the gain factor
+    $\tau$ (i.e., the "response strength"). The cross-track error is
+    defined w.r.t. reference trajectory, which here is assumed to be
+    a constant horizontal trajectory along the x-axis.   
+    
+    The proportional controller implemented here is used to direct the
+    robot motion towards the horizontal reference trajectory by giving
+    steering angle commands computed w.r.t. the normally-distributed
+    steering drift, as well as both steering- and distance measurement
+    noise.
+
+    :param x: Position of the robot along the $x$-axis.
+    :param y: Position of the robot along the $y$-axis.
+    :param orientation: Heading angle of the robot (i.e., the direction).
+    :param length:
+    :param steering_noise: Steering measurement noise sampled from a
+        Gaussian normal distribution.
+    :param distance_noise: Distance measurement noise sampled from a
+        Gaussian normal distribution.
+    :param steering_drift: Perturbation of the steering angle sampled from a
+        Gaussian normal distribution.
+    '''
+
+    def __init__(self,
+            length: float=20.0
+    ):
+        """Creates a `Robot` instance.
+
+        Initialises the robot 2D location and orientation angle to the
+        position along the origin (0, 0) with a heading of `0.0`.
+
+        :param length: Desired length to initialise the robot with. 
         """
-        Creates robot and initializes location/orientation to 0, 0, 0.
-        """
+
         self.x = 0.0
         self.y = 0.0
         self.orientation = 0.0
@@ -33,58 +67,86 @@ class Robot(object):
         self.distance_noise = 0.0
         self.steering_drift = 0.0
 
-    def set(self, x, y, orientation):
+    def set(self, 
+            x: float, 
+            y: float,
+            orientation: float
+    ):
+        """Sets the robot's 2D location and orientation to the given values.
+        
+        :param x: Position along the $x$-axis to assign to the robot.
+        :param y: Position along the $y$-axis to assign to the robot.
+        :param orientation: Heading angle to assign to the robot. 
         """
-        Sets a robot coordinate.
-        """
+
         self.x = x
         self.y = y
         self.orientation = orientation % (2.0 * np.pi)
 
-    def set_noise(self, steering_noise, distance_noise):
+    def set_noise(self, 
+            steering_noise: float, 
+            distance_noise: float
+    ):
+        """Sets the steering- and measurement- noise parameters.
+
+        This update function allows for the use of dynamic noise values,
+        which is often useful in particle filters.
+
+        :param steering_noise: Noise value to use for the steering measurement.
+        :param distance_noise: Noise value to use for the distance measurement.
         """
-        Sets the noise parameters.
-        """
-        # makes it possible to change the noise parameters
-        # this is often useful in particle filters
+
         self.steering_noise = steering_noise
         self.distance_noise = distance_noise
 
-    def set_steering_drift(self, drift):
+    def set_steering_drift(self, 
+            drift: float
+    ):
+        """Sets the systematical steering drift parameter.
+
+        :param drift: Drift value to use for the steering angle.
         """
-        Sets the systematical steering drift parameter
-        """
+
         self.steering_drift = drift
 
-    def move(self, steering, distance, tolerance=0.001, max_steering_angle=np.pi / 4.0):
+    def move(self, 
+            steering: float, 
+            distance: float, 
+            tolerance: float=0.001, 
+            max_steering_angle: float=np.pi / 4.0
+    ):
+        """Move the robot to the next time-step with the provided controls.
+
+        :param steering: Front-wheel steering angle.
+        :param distance: Total distance driven by the vehicle,
+            must be non-negative.
+        :param tolerance: Tolerance value for the manoeuvre,
+            values outside this threshold indicate a turning manoeuvre.
+        :param max_steering angle: Maximum front-wheel steering angle.
         """
-        steering = front wheel steering angle, limited by max_steering_angle
-        distance = total distance driven, most be non-negative
-        """
+
+        # Thresholding of input steering angle and distance values 
         if steering > max_steering_angle:
             steering = max_steering_angle
         if steering < -max_steering_angle:
             steering = -max_steering_angle
         if distance < 0.0:
             distance = 0.0
-
-        # apply noise
+        # Computing the noise values
         steering2 = random.gauss(steering, self.steering_noise)
         distance2 = random.gauss(distance, self.distance_noise)
-
-        # apply steering drift
+        # Applying the steering drift parameter
         steering2 += self.steering_drift
-
-        # Execute motion
+        # Compute the manoeuvre / turn motion to execute
         turn = np.tan(steering2) * distance2 / self.length
-
         if abs(turn) < tolerance:
-            # approximate by straight line motion
+            # Compute the straight-line distance approximation of the manoeuvre
             self.x += distance2 * np.cos(self.orientation)
             self.y += distance2 * np.sin(self.orientation)
             self.orientation = (self.orientation + turn) % (2.0 * np.pi)
         else:
-            # approximate bicycle model for motion
+            # Evaluate the approximate kinematic bicycle motion model
+            # for the desired turning manoeuvre
             radius = distance2 / turn
             cx = self.x - (np.sin(self.orientation) * radius)
             cy = self.y + (np.cos(self.orientation) * radius)
@@ -93,24 +155,47 @@ class Robot(object):
             self.y = cy - (np.cos(self.orientation) * radius)
 
     def __repr__(self):
+        """Overrides the default print function with vehicle state values."""
         return '[x=%.5f y=%.5f orient=%.5f]' % (self.x, self.y, self.orientation)
 
-############## ADD / MODIFY CODE BELOW ####################
-# ------------------------------------------------------------------------
-#
-# run - does a single control run
-robot = Robot()
-robot.set(0, 1, 0)
 
-def run(robot, tau, n=100, speed=1.0):
+def run(
+        robot: Robot, 
+        tau: float, 
+        n: int=100, 
+        speed: float=1.0
+) -> NoReturn:
+    """TODO. 
+    
+    Modify the `run` function here s.t. the robot position is simulated
+    across 100 time-steps. 
+    
+    The robot's steering angle should be selected using the proportional
+    gain controller implemented here w.r.t. the horizontal reference trajectory
+    and the proportional gain factor $\tau$.
+
+    :param robot: `Robot` class instance representing the vehicle to manoeuvre. 
+    :param tau: Proportional gain constant.
+    :param speed: Velocity (m/s) at which to drive the vehicle.
+    """
+
+    # The list of $x$- and $y$-values for the simulated trajectory
     x_trajectory = []
     y_trajectory = []
     # TODO: your code here
     raise NotImplementedError
-    
-x_trajectory, y_trajectory = run(robot, 0.1)
-n = len(x_trajectory)
 
-fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 8))
-ax1.plot(x_trajectory, y_trajectory, 'g', label='P controller')
-ax1.plot(x_trajectory, np.zeros(n), 'r', label='reference')
+
+if __name__ == '__main__':
+    ### Initialise the robot instance using default values
+    robot = Robot()
+    ### Perform a single control run to position (0, 1)
+    # with heading angle `0.0`
+    robot.set(0.0, 1.0, 0.0)
+    ### Execute the trajectory using the proportional controller            
+    x_trajectory, y_trajectory = run(robot, 0.1)
+    n = len(x_trajectory)
+    ### Plot the robot position and orientation relative to the reference
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 8))
+    ax1.plot(x_trajectory, y_trajectory, 'g', label='P controller')
+    ax1.plot(x_trajectory, np.zeros(n), 'r', label='reference')
