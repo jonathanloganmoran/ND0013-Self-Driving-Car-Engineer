302f7ecb714330491988be989f72f73ec8919814
jonathanloganmoran <jonathan.moran107@gmail.com>
2023-01-07 19:41:09 -0800

    :sparkles: Finishing `update_error` function

--- 5-Control/5-1-Control-Trajectory-Tracking/project/pid_controller/pid_controller.cpp
+++ 5-Control/5-1-Control-Trajectory-Tracking/project/pid_controller/pid_controller.cpp
@@ -15,9 +15,7 @@ PID::PID() {}
 PID::~PID() {}
 
 
-/* TODO.
- *
- * Initialises the PID controller parameters with the given values.
+/* Initialises the PID controller parameters with the given values.
  *
  * @param  k_p             Gain value to use for the proportional term.
  * @param  k_i             Gain value to use for the integral term.
@@ -32,19 +30,39 @@ void PID::init_controller(
     double lim_max_output, 
     double lim_min_output
 ) {
+  this->k_p = k_p;
+  this->k_i = k_i;
+  this->k_d = k_d;
+  this->lim_max_output = lim_max_output;
+  this->lim_min_output = lim_min_output;
+  this->delta_t = 0.0;
+  this->error_p = 0.0;
+  this->error_i = 0.0;
+  this->error_d = 0.0;
 }
 
 
-/* TODO.
+/* Evaluates the PID controller expression using the given cross-track error.
  * 
- * Updates the PID errors based on the given cross-track error value.
- * 
- * @param  cte
+ * @param  cte    Current cross-track error value. 
  * 
  */
 void PID::update_error(
     double cte
 ) {
+  // Update the proportional-gain error
+  this->error_p = cte;
+  // Update the derivative-gain error term
+  if (this->delta_t > 0.0) {
+    // The $\dot{cte}$ term, i.e., the derivative of CTE w.r.t. $\Delta t$
+    this->error_d = (cte - error_p) / this->delta_t;
+  }
+  else {
+    // Divide by zero: set resulting derivative error to 0.0
+    this->error_d = 0.0; 
+  }
+  // Update the integral-gain error term
+  this->error_i += cte * this->delta_t;
 }
 
 
