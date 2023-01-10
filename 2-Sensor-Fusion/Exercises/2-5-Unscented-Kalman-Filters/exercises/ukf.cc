8d2bdcb20aad620d60cc138cca7b0f7fe38f3b8b
jonathanloganmoran <jonathan.moran107@gmail.com>
2023-01-10 15:11:30 -0800

    :art::bulb: Adding descriptive docstrings and enforcing some Google C++ style guide principles

--- 2-Sensor-Fusion/Exercises/2-5-Unscented-Kalman-Filters/exercises/ukf.cc
+++ 2-Sensor-Fusion/Exercises/2-5-Unscented-Kalman-Filters/exercises/ukf.cc
@@ -1,80 +1,86 @@
+/* ----------------------------------------------------------------------------
+ * Lesson "2.5: Unscented Kalman Filters"
+ * Authors     : Dominik Nuss, Andrei Vatavu.
+ *
+ * Modified by : Jonathan L. Moran (jonathan.moran107@gmail.com)
+ *
+ * Purpose of this file: Implements the Unscented Kalman Filter (UKF).
+ * ----------------------------------------------------------------------------
+ */
+
 #include "ukf.h"
 #include <iostream>
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
-UKF::UKF() {
-  Init();
-}
 
-UKF::~UKF() {
-
-}
-
-void UKF::Init() {
-
-}
-
-/**
- * Programming assignment functions: 
+/* Initialise the Unscented Kalman Filter instance with default values.
  */
-void UKF::GenerateSigmaPoints(MatrixXd* Xsig_out) {
+UKF::UKF() {
+  InitFilter();
+}
 
-  // set state dimension
+// Destructor
+UKF::~UKF() {}
+// Constructor
+void UKF::InitFilter() {}
+
+
+/* Constructs the sigma point matrix.
+ *
+ * Implements the sigma point estimation for use in the prediction step.
+ * The UKF performs an unscented transformation which approximates a normal
+ * distribution from the non-linear posterior state estimation.
+ *
+ * Here the sigma points are determined with respect to the dimension of the
+ * state vector and a spreading factor $\lambda$. This design parameter
+ * governs how "close" a sigma point is to the mean $x_{k\vert k}$, i.e.,
+ * the posterior state estimation from the previous time-step. The posterior
+ * covariance matrix $\mathrm{P}_{k\vert k}$ is also used in the calculation
+ * of the sigma points.
+ * 
+ * @param  Xsig_out   Column-vector matrix to store resulting sigma points.
+ */
+void UKF::GenerateSigmaPoints(
+    Eigen::MatrixXd* Xsig_out
+) {
+  // Set the state dimension
   int n_x = 5;
-
-  // define spreading parameter
+  // Define the spreading parameter
   double lambda = 3 - n_x;
-
-  // set example state
-  VectorXd x = VectorXd(n_x);
-  x <<   5.7441,
-         1.3800,
-         2.2049,
-         0.5015,
-         0.3528;
-
-  // set example covariance matrix
-  MatrixXd P = MatrixXd(n_x, n_x);
-  P <<     0.0043,   -0.0013,    0.0030,   -0.0022,   -0.0020,
-          -0.0013,    0.0077,    0.0011,    0.0071,    0.0060,
-           0.0030,    0.0011,    0.0054,    0.0007,    0.0008,
-          -0.0022,    0.0071,    0.0007,    0.0098,    0.0100,
-          -0.0020,    0.0060,    0.0008,    0.0100,    0.0123;
-
-  // create sigma point matrix
-  MatrixXd Xsig = MatrixXd(n_x, 2 * n_x + 1);
-
-  // calculate square root of P
-  MatrixXd A = P.llt().matrixL();
-
+  // Set the state vector
+  Eigen::VectorXd x = Eigen::VectorXd(n_x);
+  x << 5.7441,
+       1.3800,
+       2.2049,
+       0.5015,
+       0.3528;
+  // Set the covariance matrix
+  Eigen::MatrixXd P = Eigen::MatrixXd(n_x, n_x);
+  P << 0.0043,   -0.0013,    0.0030,   -0.0022,   -0.0020,
+      -0.0013,    0.0077,    0.0011,    0.0071,    0.0060,
+       0.0030,    0.0011,    0.0054,    0.0007,    0.0008,
+      -0.0022,    0.0071,    0.0007,    0.0098,    0.0100,
+      -0.0020,    0.0060,    0.0008,    0.0100,    0.0123;
+  // Create the sigma point matrix
+  Eigen::MatrixXd Xsig = Eigen::MatrixXd(
+      n_x, 
+      2 * n_x + 1
+  );
+  // Calculate square-root of matrix `P`
+  Eigen::MatrixXd A = P.llt().matrixL();
   /**
    * Student part begin
    */
-
   // your code goes here 
-  // calculate sigma points ...
-
-  // set sigma points as columns of matrix Xsig
-
+  // Calculate the set of sigma points
+  // Set the sigma points as the columns of matrix `Xsig`
   /**
    * Student part end
    */
-
-  // print result
-  // std::cout << "Xsig = " << std::endl << Xsig << std::endl;
-
-  // write result
+  // Print the resulting matrix
+  // std::cout << "Xsig = " << std::endl << Xsig << "\n";
+  // Write the result to the output matrix
   *Xsig_out = Xsig;
 }
-
-/**
- * expected result:
- * Xsig =
- *  5.7441  5.85768   5.7441   5.7441   5.7441   5.7441  5.63052   5.7441   5.7441   5.7441   5.7441
- *    1.38  1.34566  1.52806     1.38     1.38     1.38  1.41434  1.23194     1.38     1.38     1.38
- *  2.2049  2.28414  2.24557  2.29582   2.2049   2.2049  2.12566  2.16423  2.11398   2.2049   2.2049
- *  0.5015  0.44339 0.631886 0.516923 0.595227   0.5015  0.55961 0.371114 0.486077 0.407773   0.5015
- *  0.3528 0.299973 0.462123 0.376339  0.48417 0.418721 0.405627 0.243477 0.329261  0.22143 0.286879
- */
--- 2-Sensor-Fusion/Exercises/2-5-Unscented-Kalman-Filters/exercises/ukf.h
+++ 2-Sensor-Fusion/Exercises/2-5-Unscented-Kalman-Filters/exercises/ukf.h
@@ -1,37 +1,50 @@
+/* ----------------------------------------------------------------------------
+ * Lesson "2.5: Unscented Kalman Filters"
+ * Authors     : Dominik Nuss, Andrei Vatavu.
+ *
+ * Modified by : Jonathan L. Moran (jonathan.moran107@gmail.com)
+ *
+ * Purpose of this file: Header file for the Unscented Kalman Filter (UKF).
+ * ----------------------------------------------------------------------------
+ */
+
 #ifndef UKF_H
 #define UKF_H
 
 #include "Dense"
 
+
+/* The Unscented Kalman Filter (UKF) class.
+ */
 class UKF {
  public:
-  /**
-   * Constructor
-   */
   UKF();
-
-  /**
-   * Destructor
-   */
   virtual ~UKF();
-
-  /**
-   * Init Initializes Unscented Kalman filter
-   */
-  void Init();
-
-  /**
-   * Student assignment functions
-   */
-  void GenerateSigmaPoints(Eigen::MatrixXd* Xsig_out);
-  void AugmentedSigmaPoints(Eigen::MatrixXd* Xsig_out);
-  void SigmaPointPrediction(Eigen::MatrixXd* Xsig_out);
-  void PredictMeanAndCovariance(Eigen::VectorXd* x_pred, 
-                                Eigen::MatrixXd* P_pred);
-  void PredictRadarMeasurement(Eigen::VectorXd* z_out, 
-                               Eigen::MatrixXd* S_out);
-  void UpdateState(Eigen::VectorXd* x_out, 
-                   Eigen::MatrixXd* P_out);
+  // Initalises the UKF instance
+  void InitFilter();
+  // Computes the sigma points
+  void GenerateSigmaPoints(
+      Eigen::MatrixXd* Xsig_out
+  );
+  void AugmentedSigmaPoints(
+      Eigen::MatrixXd* Xsig_out
+  );
+  void SigmaPointPrediction(
+      Eigen::MatrixXd* Xsig_out
+  );
+  void PredictMeanAndCovariance(
+      Eigen::VectorXd* x_pred, 
+      Eigen::MatrixXd* P_pred
+  );
+  void PredictRadarMeasurement(
+      Eigen::VectorXd* z_out, 
+      Eigen::MatrixXd* S_out
+  );
+  void UpdateState(
+      Eigen::VectorXd* x_out, 
+      Eigen::MatrixXd* P_out
+  );
 };
+
 
 #endif  // UKF_H
