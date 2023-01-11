3b50c8a74279f4006b22793658d7e5ce4a719432
jonathanloganmoran <jonathan.moran107@gmail.com>
2023-01-11 13:39:36 -0800

    :tada: Adding starter code for Extracirricular Exercise 2.5.3: Prediction Step with Sigma Points

--- 2-Sensor-Fusion/Exercises/2-5-Unscented-Kalman-Filters/exercises/ukf.cc
+++ 2-Sensor-Fusion/Exercises/2-5-Unscented-Kalman-Filters/exercises/ukf.cc
@@ -190,3 +190,53 @@ void UKF::AugmentedSigmaPoints(
   // Write the result to the output matrix
   *Xsig_out = Xsig_aug; 
 }
+
+
+/**
+ * Programming assignment functions: 
+ */
+
+void UKF::SigmaPointPrediction(MatrixXd* Xsig_out) {
+
+  // set state dimension
+  int n_x = 5;
+
+  // set augmented dimension
+  int n_aug = 7;
+
+  // create example sigma point matrix
+  MatrixXd Xsig_aug = MatrixXd(n_aug, 2 * n_aug + 1);
+  Xsig_aug <<
+    5.7441,  5.85768,   5.7441,   5.7441,   5.7441,   5.7441,   5.7441,   5.7441,   5.63052,   5.7441,   5.7441,   5.7441,   5.7441,   5.7441,   5.7441,
+      1.38,  1.34566,  1.52806,     1.38,     1.38,     1.38,     1.38,     1.38,   1.41434,  1.23194,     1.38,     1.38,     1.38,     1.38,     1.38,
+    2.2049,  2.28414,  2.24557,  2.29582,   2.2049,   2.2049,   2.2049,   2.2049,   2.12566,  2.16423,  2.11398,   2.2049,   2.2049,   2.2049,   2.2049,
+    0.5015,  0.44339, 0.631886, 0.516923, 0.595227,   0.5015,   0.5015,   0.5015,   0.55961, 0.371114, 0.486077, 0.407773,   0.5015,   0.5015,   0.5015,
+    0.3528, 0.299973, 0.462123, 0.376339,  0.48417, 0.418721,   0.3528,   0.3528,  0.405627, 0.243477, 0.329261,  0.22143, 0.286879,   0.3528,   0.3528,
+         0,        0,        0,        0,        0,        0,  0.34641,        0,         0,        0,        0,        0,        0, -0.34641,        0,
+         0,        0,        0,        0,        0,        0,        0,  0.34641,         0,        0,        0,        0,        0,        0, -0.34641;
+
+  // create matrix with predicted sigma points as columns
+  MatrixXd Xsig_pred = MatrixXd(n_x, 2 * n_aug + 1);
+
+  double delta_t = 0.1; // time diff in sec
+
+  /**
+   * Student part begin
+   */
+
+  // predict sigma points
+
+  // avoid division by zero
+
+  // write predicted sigma points into right column
+
+  /**
+   * Student part end
+   */
+
+  // print result
+  std::cout << "Xsig_pred = " << std::endl << Xsig_pred << std::endl;
+
+  // write result
+  *Xsig_out = Xsig_pred;
+}
