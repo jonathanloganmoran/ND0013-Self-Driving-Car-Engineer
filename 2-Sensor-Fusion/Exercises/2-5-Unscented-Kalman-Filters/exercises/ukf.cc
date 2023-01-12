5ccd895efafb3c5ebc891aa0f44918df49207188
jonathanloganmoran <jonathan.moran107@gmail.com>
2023-01-12 09:21:16 -0800

    :art::bulb: Adding descriptive docstrings and enforcing some Google C++ style guide principles

--- 2-Sensor-Fusion/Exercises/2-5-Unscented-Kalman-Filters/exercises/ukf.cc
+++ 2-Sensor-Fusion/Exercises/2-5-Unscented-Kalman-Filters/exercises/ukf.cc
@@ -287,61 +287,64 @@ void UKF::SigmaPointPrediction(
   *Xsig_out = Xsig_pred;
 }
 
-/**
- * Programming assignment functions: 
+
+/* Constructs the predicted the mean state estimation and covariance matrix.
+ *
+ * The mean state vector and covariance matrix are predicted into the next
+ * time-step using the sigma point prediction matrix from the previous step.
+ * The predicted values are computed w.r.t. a weight vector which is defined
+ * in terms of the spreading parameter $\lambda$. The weights are applied to
+ * "undo" the effect of spreading on the covariance and mean state prediction.
+ * 
+ * @param  x_out  Predicted mean state estimation. 
+ * @param  P_out  Predicted covariance matrix.
  */
-
-void UKF::PredictMeanAndCovariance(VectorXd* x_out, MatrixXd* P_out) {
-
-  // set state dimension
+void UKF::PredictMeanAndCovariance(
+    Eigen::VectorXd* x_out, 
+    Eigen::MatrixXd* P_out
+) {
+  /*** Set the state variables (values should match across functions) ***/
+  // Set the state dimension
   int n_x = 5;
-
-  // set augmented dimension
-  int n_aug = 7;
-
-  // define spreading parameter
-  double lambda = 3 - n_aug;
-
-  // create example matrix with predicted sigma points
-  MatrixXd Xsig_pred = MatrixXd(n_x, 2 * n_aug + 1);
-  Xsig_pred <<
-         5.9374,  6.0640,   5.925,  5.9436,  5.9266,  5.9374,  5.9389,  5.9374,  5.8106,  5.9457,  5.9310,  5.9465,  5.9374,  5.9359,  5.93744,
-           1.48,  1.4436,   1.660,  1.4934,  1.5036,    1.48,  1.4868,    1.48,  1.5271,  1.3104,  1.4787,  1.4674,    1.48,  1.4851,    1.486,
-          2.204,  2.2841,  2.2455,  2.2958,   2.204,   2.204,  2.2395,   2.204,  2.1256,  2.1642,  2.1139,   2.204,   2.204,  2.1702,   2.2049,
-         0.5367, 0.47338, 0.67809, 0.55455, 0.64364, 0.54337,  0.5367, 0.53851, 0.60017, 0.39546, 0.51900, 0.42991, 0.530188,  0.5367, 0.535048,
-          0.352, 0.29997, 0.46212, 0.37633,  0.4841, 0.41872,   0.352, 0.38744, 0.40562, 0.24347, 0.32926,  0.2214, 0.28687,   0.352, 0.318159;
-
-  // create vector for weights
-  VectorXd weights = VectorXd(2*n_aug+1);
-  
-  // create vector for predicted state
-  VectorXd x = VectorXd(n_x);
-
-  // create covariance matrix for prediction
-  MatrixXd P = MatrixXd(n_x, n_x);
-
-
+  // Set the augmented dimension
+  // Process noise $\nu_{k}$ has the terms $\nu_{a, k}$, $\nu_{\ddot{psi},k}$
+  int n_a = 2;
+  // The process noise dimension added to the state vector dimension
+  int n_aug = n_x + n_a;
+  // Calculate the number of sigma points to compute
+  int n_sigma_points = 2 * n_aug + 1;
+  // Get the augmented sigma point matrix
+  Eigen::MatrixXd Xsig_aug(n_aug, n_sigma_points);
+  AugmentedSigmaPoints(&Xsig_aug);
+  /*** Compute the predicted sigma point matrix ***/
+  // Instantiate the predicted sigma point matrix
+  Eigen::MatrixXd Xsig_pred(n_x, n_sigma_points);
+  // Define the delta-time variable (s)
+  double delta_t = 0.1;
+  // Get the predicted sigma points
+  SigmaPointPrediction(&Xsig_pred);
+  /*** Compute the predicted mean state and covariance matrix ***/
+  // Instantiate the weight vector
+  Eigen::VectorXd weights(n_sigma_points, 1);
+  // Instantiate the predicted mean state vector
+  Eigen::VectorXd x(n_x);
+  // Instantiate the predicted covariance matrix
+  Eigen::MatrixXd P(n_x, n_x);
   /**
    * Student part begin
    */
-
-  // set weights
-
-  // predict state mean
-
-  // predict state covariance matrix
-
+  // Set the weight vector values
+  // Perform the mean state vector prediction
+  // Perform the state covariance matrix prediction
   /**
    * Student part end
    */
-
-  // print result
-  std::cout << "Predicted state" << std::endl;
-  std::cout << x << std::endl;
-  std::cout << "Predicted covariance matrix" << std::endl;
-  std::cout << P << std::endl;
-
-  // write result
+  // Print the resulting mean state and covariance matrix predictions
+  std::cout << "Predicted state" << "\n";
+  std::cout << x << "\n";
+  std::cout << "Predicted covariance matrix" << "\n";
+  std::cout << P << "\n";
+  // Write the result to the input variables
   *x_out = x;
   *P_out = P;
 }
