842ae3cff4f39f660ee622d05eb0c87e586ea5ed
jonathanloganmoran <jonathan.moran107@gmail.com>
2023-01-12 15:51:38 -0800

    :sparkles: Adding test case for Extracirricular Exercise 2.5.6: Innovation Step with State and Covariance Update

--- 2-Sensor-Fusion/Exercises/2-5-Unscented-Kalman-Filters/exercises/5_tests.cc
+++ 2-Sensor-Fusion/Exercises/2-5-Unscented-Kalman-Filters/exercises/5_tests.cc
@@ -236,6 +236,103 @@ void test_predict_radar_measurement() {
 }
 
 
+void test_update_state() {
+  // Create the Unscented Kalman Filter (UKF) instance
+  UKF ukf;
+  /*** Set the state variables (values should match across functions) ***/
+  // Set the state dimension
+  int n_x = 5;
+  // Set the augmented dimension
+  // Process noise $\nu_{k}$ has the terms $\nu_{a, k}$, $\nu_{\ddot{psi},k}$
+  int n_a = 2;
+  // The process noise dimension added to the state vector dimension
+  int n_aug = n_x + n_a;
+  // Calculate the number of sigma points to compute
+  int n_sigma_points = 2 * n_aug + 1;
+  // Set the measurement dimensions
+  // Note: the radar measurement vector is $[\rho, \phi, \dot{\rho}]$
+  // i.e., the measured radial distance, angle, and radial velocity
+  int n_z = 3;
+  // Define the spreading parameter
+  double lambda = 3 - n_aug;
+  // Define the delta-time variable (s)
+  double delta_t = 0.1;
+  // Define the incoming radar measurement vector
+  // assumed to be the same in `UpdateState`
+  Eigen::VectorXd z(n_z);
+  z <<
+     5.9214,   // rho in m
+     0.2187,   // phi in rad
+     2.0062;   // rho_dot in m/s
+  // Instantiate the updated state estimation
+  Eigen::VectorXd x(n_x, 1);
+  // Instantiate the updated covariance matrix
+  Eigen::MatrixXd P(n_x, n_x);
+  /*** Define the variables needed for the test case ***/
+  // NOTE: the following values must be set inside `UpdateState`
+  // in order for the expected test case values to hold
+  // Eigen::MatrixXd Xsig_pred(n_x, n_sigma_points);
+  // Xsig_pred <<
+  //    5.9374,  6.0640,   5.925,  5.9436,  5.9266,  5.9374,  5.9389,  5.9374,  5.8106,  5.9457,  5.9310,  5.9465,  5.9374,  5.9359,  5.93744,
+  //      1.48,  1.4436,   1.660,  1.4934,  1.5036,    1.48,  1.4868,    1.48,  1.5271,  1.3104,  1.4787,  1.4674,    1.48,  1.4851,    1.486,
+  //     2.204,  2.2841,  2.2455,  2.2958,   2.204,   2.204,  2.2395,   2.204,  2.1256,  2.1642,  2.1139,   2.204,   2.204,  2.1702,   2.2049,
+  //    0.5367, 0.47338, 0.67809, 0.55455, 0.64364, 0.54337,  0.5367, 0.53851, 0.60017, 0.39546, 0.51900, 0.42991, 0.530188,  0.5367, 0.535048,
+  //     0.352, 0.29997, 0.46212, 0.37633,  0.4841, 0.41872,   0.352, 0.38744, 0.40562, 0.24347, 0.32926,  0.2214, 0.28687,   0.352, 0.318159;
+  // Eigen::VectorXd x(n_x);
+  // x <<
+  //    5.93637,
+  //    1.49035,
+  //    2.20528,
+  //   0.536853,
+  //   0.353577;
+  // Eigen::MatrixXd P(n_x, n_x);
+  // P <<
+  //   0.0054342,  -0.002405,  0.0034157, -0.0034819, -0.00299378,
+  //   -0.002405,    0.01084,   0.001492,  0.0098018,  0.00791091,
+  //   0.0034157,   0.001492,  0.0058012, 0.00077863, 0.000792973,
+  //  -0.0034819,  0.0098018, 0.00077863,   0.011923,   0.0112491,
+  //  -0.0029937,  0.0079109, 0.00079297,   0.011249,   0.0126972;
+  // Eigen::MatrixXd Zsig(n_z, n_sigma_points);
+  // Zsig <<
+  //   6.1190,  6.2334,  6.1531,  6.1283,  6.1143,  6.1190,  6.1221,  6.1190,  6.0079,  6.0883,  6.1125,  6.1248,  6.1190,  6.1188,  6.12057,
+  //  0.24428,  0.2337, 0.27316, 0.24616, 0.24846, 0.24428, 0.24530, 0.24428, 0.25700, 0.21692, 0.24433, 0.24193, 0.24428, 0.24515, 0.245239,
+  //   2.1104,  2.2188,  2.0639,   2.187,  2.0341,  2.1061,  2.1450,  2.1092,  2.0016,   2.129,  2.0346,  2.1651,  2.1145,  2.0786,  2.11295;
+  // Eigen::VectorXd z_pred(n_z);
+  // z_pred <<
+  //   6.12155,
+  //    0.245993,
+  //    2.10313;
+  // Eigen::MatrixXd S(n_z, n_z);
+  // S <<
+  //     0.0946171, -0.000139448,   0.00407016,
+  //  -0.000139448,  0.000617548, -0.000770652,
+  //    0.00407016, -0.000770652,    0.0180917;
+  /*** Perform the UKF Innovation step ***/
+  ukf.UpdateState(x, P);
+  // Perform the L2 norm to compare the output values
+  Eigen::VectorXd x_expected(5, 1);
+  x_expected <<
+    5.92276,
+    1.41823,
+    2.15593,
+    0.489274,
+    0.321338;
+  Eigen::MatrixXd P_expected(5, 5);
+  P_expected <<
+    0.00361579, -0.000357881, 0.00208316, -0.000937196, -0.00071727,
+   -0.000357881, 0.00539867,  0.00156846,  0.00455342,   0.00358885,
+    0.00208316,  0.00156846,  0.00410651,  0.00160333,   0.00171811,
+   -0.000937196, 0.00455342,  0.00160333,  0.00652634,   0.00669436,
+   -0.00071719,  0.00358884,  0.00171811,  0.00669426,   0.00881797;
+  // Precision (i.e., max allowed magnitude of the outputs' L2 difference)
+  // NOTE: see above caveat (test works only when `Xsig_pred` is set manually) 
+  double epsilon = 0.0001;
+  std::cout << "Result `x` matches expected amount by `epsilon = " << epsilon << '`';
+  std::cout << ": " << std::boolalpha << x.isApprox(x_expected, epsilon) << "\n";
+  std::cout << "Result `P` matches expected amount by `epsilon = " << epsilon << '`';
+  std::cout << ": " << std::boolalpha << P.isApprox(P_expected, epsilon) << "\n";
+}
+
 
 int main() {
   // Exercise 2.5.1: Generating Sigma Points
