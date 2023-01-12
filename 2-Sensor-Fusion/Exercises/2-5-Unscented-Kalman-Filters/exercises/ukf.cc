275724b169e28e1ed68e6bdbfd8fd8091cf091c4
jonathanloganmoran <jonathan.moran107@gmail.com>
2023-01-12 14:38:56 -0800

    :art::bulb: Adding descriptive docstrings and enforcing some Google C++ style guide principles

--- 2-Sensor-Fusion/Exercises/2-5-Unscented-Kalman-Filters/exercises/ukf.cc
+++ 2-Sensor-Fusion/Exercises/2-5-Unscented-Kalman-Filters/exercises/ukf.cc
@@ -541,111 +541,160 @@ void UKF::PredictRadarMeasurement(
 }
 
 
-/**
- * Programming assignment functions: 
+/* Performs the UKF Innovation step to update the state mean and covariance.
+ * 
+ * The Innovation step of the Unscented Kalman Filter (UKF) differs from
+ * the standard Kalman filter (KF) only by a cross-correlation term
+ * computed between the state- and measurement space of the sigma points.
+ * This term is used in the equation for the Kalman gain given by:
+ * $$\begin{align}
+ * \mathrm{K}_{k+1\vert k} &= \mathrm{T}_{k+1\vert k} 
+ *                            * \mathrm{S}^{\inv}_{k+1\vert k}. 
+ * \end{align}$$
+ * 
+ * The state update equation for the time-step $k+1$ is given by:
+ * $$\begin{align}
+ * x_{k+1\vert k+1} = x_{k+1\vert k}
+ *                    + \mathrm{K}_{k+1\vert k}
+ *                    * (z_{k+1} - z_{k+1\vert k}).
+ * \end{align}$$
+ * 
+ * The covariance matrix update equation for time-step $k+1$ is given by:
+ * $$\begin{align}
+ * \mathrm{P}_{k+1\vert k+1} = \mathrm{P}_{k+1\vert k}
+ *                             - \mathrm{K}_{k+1 \ k}
+ *                             * \mathrm{S}_{k+1 \vert k}
+ *                             * \mathrm{K}^{\top}_{k+1 \vert k}.
+ * \end{align}$$
+ * 
+ * The cross-correlation matrix between the sigma points in state- and
+ * measurement-space is given as:
+ * $$\begin{align}
+ * \mathrm{T}_{k+1\vert k} = 
+ * \sum_{i=0}^{2*n_{a}} w_{i} 
+ *                      * (\mathcal{x}_{k+1\vert k,i} - x_{k+1\vert k})
+ *                      * (\mathcal{z}_{k+1\vert k,i} - z_{k+1\vert k})^{\top}.
+ * \end{align}$$
+ * 
+ * @param  x_out    Vector to store the updated mean state estimate. 
+ * @param  P_out    Matrix to store the updated covariance matrix estimate.
  */
-
-void UKF::UpdateState(VectorXd* x_out, MatrixXd* P_out) {
-
-  // set state dimension
+void UKF::UpdateState(
+    Eigen::VectorXd* x_out,
+    Eigen::MatrixXd* P_out
+) {
+ /*** Set the state variables (values should match across functions) ***/
+  // Set the state dimension
   int n_x = 5;
-
-  // set augmented dimension
-  int n_aug = 7;
-
-  // set measurement dimension, radar can measure r, phi, and r_dot
+  // Set the augmented dimension
+  // Process noise $\nu_{k}$ has the terms $\nu_{a, k}$, $\nu_{\ddot{psi},k}$
+  int n_a = 2;
+  // The process noise dimension added to the state vector dimension
+  int n_aug = n_x + n_a;
+  // Calculate the number of sigma points to compute
+  int n_sigma_points = 2 * n_aug + 1;
+  // Set the measurement dimensions
+  // Note: the radar measurement vector is $[\rho, \phi, \dot{\rho}]$
+  // i.e., the measured radial distance, angle, and radial velocity
   int n_z = 3;
-
-  // define spreading parameter
+  // Define the spreading parameter
   double lambda = 3 - n_aug;
-
-  // set vector for weights
-  VectorXd weights = VectorXd(2*n_aug+1);
-  double weight_0 = lambda/(lambda+n_aug);
-  double weight = 0.5/(lambda+n_aug);
-  weights(0) = weight_0;
-
-  for (int i=1; i<2*n_aug+1; ++i) {  
-    weights(i) = weight;
+  // Set the weight vector values
+  Eigen::VectorXd w(n_sigma_points, 1);
+  w(0) = lambda / (lambda + n_aug);
+  double weight = 1.0 / (2.0 * (lambda + n_aug));
+  for (int i = 1; i < n_sigma_points; ++i) {  
+    w(i) = weight;
   }
-
-  // create example matrix with predicted sigma points in state space
-  MatrixXd Xsig_pred = MatrixXd(n_x, 2 * n_aug + 1);
-  Xsig_pred <<
-     5.9374,  6.0640,   5.925,  5.9436,  5.9266,  5.9374,  5.9389,  5.9374,  5.8106,  5.9457,  5.9310,  5.9465,  5.9374,  5.9359,  5.93744,
-       1.48,  1.4436,   1.660,  1.4934,  1.5036,    1.48,  1.4868,    1.48,  1.5271,  1.3104,  1.4787,  1.4674,    1.48,  1.4851,    1.486,
-      2.204,  2.2841,  2.2455,  2.2958,   2.204,   2.204,  2.2395,   2.204,  2.1256,  2.1642,  2.1139,   2.204,   2.204,  2.1702,   2.2049,
-     0.5367, 0.47338, 0.67809, 0.55455, 0.64364, 0.54337,  0.5367, 0.53851, 0.60017, 0.39546, 0.51900, 0.42991, 0.530188,  0.5367, 0.535048,
-      0.352, 0.29997, 0.46212, 0.37633,  0.4841, 0.41872,   0.352, 0.38744, 0.40562, 0.24347, 0.32926,  0.2214, 0.28687,   0.352, 0.318159;
-
-  // create example vector for predicted state mean
-  VectorXd x = VectorXd(n_x);
-  x <<
-     5.93637,
-     1.49035,
-     2.20528,
-    0.536853,
-    0.353577;
-
-  // create example matrix for predicted state covariance
-  MatrixXd P = MatrixXd(n_x,n_x);
-  P <<
-    0.0054342,  -0.002405,  0.0034157, -0.0034819, -0.00299378,
-    -0.002405,    0.01084,   0.001492,  0.0098018,  0.00791091,
-    0.0034157,   0.001492,  0.0058012, 0.00077863, 0.000792973,
-   -0.0034819,  0.0098018, 0.00077863,   0.011923,   0.0112491,
-   -0.0029937,  0.0079109, 0.00079297,   0.011249,   0.0126972;
-
-  // create example matrix with sigma points in measurement space
-  MatrixXd Zsig = MatrixXd(n_z, 2 * n_aug + 1);
-  Zsig <<
-    6.1190,  6.2334,  6.1531,  6.1283,  6.1143,  6.1190,  6.1221,  6.1190,  6.0079,  6.0883,  6.1125,  6.1248,  6.1190,  6.1188,  6.12057,
-   0.24428,  0.2337, 0.27316, 0.24616, 0.24846, 0.24428, 0.24530, 0.24428, 0.25700, 0.21692, 0.24433, 0.24193, 0.24428, 0.24515, 0.245239,
-    2.1104,  2.2188,  2.0639,   2.187,  2.0341,  2.1061,  2.1450,  2.1092,  2.0016,   2.129,  2.0346,  2.1651,  2.1145,  2.0786,  2.11295;
-
-  // create example vector for mean predicted measurement
-  VectorXd z_pred = VectorXd(n_z);
-  z_pred <<
-      6.12155,
-     0.245993,
-      2.10313;
-
-  // create example matrix for predicted measurement covariance
-  MatrixXd S = MatrixXd(n_z,n_z);
-  S <<
-      0.0946171, -0.000139448,   0.00407016,
-   -0.000139448,  0.000617548, -0.000770652,
-     0.00407016, -0.000770652,    0.0180917;
-
-  // create example vector for incoming radar measurement
-  VectorXd z = VectorXd(n_z);
-  z <<
-     5.9214,   // rho in m
-     0.2187,   // phi in rad
-     2.0062;   // rho_dot in m/s
-
-  // create matrix for cross correlation Tc
-  MatrixXd Tc = MatrixXd(n_x, n_z);
-
+  /*** Compute the predicted sigma point matrix ***/
+  // Define the delta-time variable (s)
+  double delta_t = 0.1;
+  // Instantiate the predicted sigma point matrix
+  // NOTE: if running unit test in `5_tests.cc`, the values of the matrix
+  // must be set manually using the provided definition
+  Eigen::MatrixXd Xsig_pred(n_x, n_sigma_points);
+  // Xsig_pred <<
+  //    5.9374,  6.0640,   5.925,  5.9436,  5.9266,  5.9374,  5.9389,  5.9374,  5.8106,  5.9457,  5.9310,  5.9465,  5.9374,  5.9359,  5.93744,
+  //      1.48,  1.4436,   1.660,  1.4934,  1.5036,    1.48,  1.4868,    1.48,  1.5271,  1.3104,  1.4787,  1.4674,    1.48,  1.4851,    1.486,
+  //     2.204,  2.2841,  2.2455,  2.2958,   2.204,   2.204,  2.2395,   2.204,  2.1256,  2.1642,  2.1139,   2.204,   2.204,  2.1702,   2.2049,
+  //    0.5367, 0.47338, 0.67809, 0.55455, 0.64364, 0.54337,  0.5367, 0.53851, 0.60017, 0.39546, 0.51900, 0.42991, 0.530188,  0.5367, 0.535048,
+  //     0.352, 0.29997, 0.46212, 0.37633,  0.4841, 0.41872,   0.352, 0.38744, 0.40562, 0.24347, 0.32926,  0.2214, 0.28687,   0.352, 0.318159;
+  // Get the predicted sigma points
+  SigmaPointPrediction(&Xsig_pred);
+  /*** Compute the predicted mean state and covariance matrix ***/
+  // Initialise the predicted mean state vector
+  Eigen::VectorXd x(n_x);
+  x.fill(0.0);
+  // NOTE: if running unit test in `5_tests.cc`, the values of the vector
+  // must be set manually using the provided definition
+  // x <<
+  //    5.93637,
+  //    1.49035,
+  //    2.20528,
+  //   0.536853,
+  //   0.353577;
+  // Initialise the predicted covariance matrix
+  Eigen::MatrixXd P(n_x, n_x);
+  P.fill(0.0);
+  // NOTE: if running unit test in `5_tests.cc`, the values of the matrix
+  // must be set manually using the provided definition
+  // P <<
+  //   0.0054342,  -0.002405,  0.0034157, -0.0034819, -0.00299378,
+  //   -0.002405,    0.01084,   0.001492,  0.0098018,  0.00791091,
+  //   0.0034157,   0.001492,  0.0058012, 0.00077863, 0.000792973,
+  //  -0.0034819,  0.0098018, 0.00077863,   0.011923,   0.0112491,
+  //  -0.0029937,  0.0079109, 0.00079297,   0.011249,   0.0126972;
+  PredictMeanAndCovariance(x, P);
+  /*** Obtain the measurement vector and covariance matrix estimations ***/
+  // Instantiate the measurement matrix
+  Eigen::MatrixXd Zsig(n_z, n_sigma_points);
+  // NOTE: if running unit test in `5_tests.cc`, the values of the matrix
+  // must be set manually using the provided definition
+  // Zsig <<
+  //   6.1190,  6.2334,  6.1531,  6.1283,  6.1143,  6.1190,  6.1221,  6.1190,  6.0079,  6.0883,  6.1125,  6.1248,  6.1190,  6.1188,  6.12057,
+  //  0.24428,  0.2337, 0.27316, 0.24616, 0.24846, 0.24428, 0.24530, 0.24428, 0.25700, 0.21692, 0.24433, 0.24193, 0.24428, 0.24515, 0.245239,
+  //   2.1104,  2.2188,  2.0639,   2.187,  2.0341,  2.1061,  2.1450,  2.1092,  2.0016,   2.129,  2.0346,  2.1651,  2.1145,  2.0786,  2.11295;
+  // Instantiate the mean measurement vector
+  Eigen::VectorXd z_pred(n_z);
+  // NOTE: if running unit test in `5_tests.cc`, the values of the vector
+  // must be set manually using the provided definition
+  // z_pred <<
+  //   6.12155,
+  //    0.245993,
+  //    2.10313;
+  // Instantiate the predicted measurement covariance matrix
+  Eigen::MatrixXd S(n_z, n_z);
+  // NOTE: if running unit test in `5_tests.cc`, the values of the matrix
+  // must be set manually using the provided definition
+  // S <<
+  //     0.0946171, -0.000139448,   0.00407016,
+  //  -0.000139448,  0.000617548, -0.000770652,
+  //    0.00407016, -0.000770652,    0.0180917;
+  // Instantiate the incoming radar measurement vector
+  Eigen::VectorXd z(n_z);
+  // NOTE: if running unit test in `5_tests.cc`, the values of the vector
+  // must be set manually using the provided definition
+  // z <<
+  //    5.9214,   // rho in m
+  //    0.2187,   // phi in rad
+  //    2.0062;   // rho_dot in m/s
+  /*** Compute the updated state and covariance (innovation step) ***/
+  // Instantiate the cross-correlation matrix
+  Eigen::MatrixXd Tc(n_x, n_z);
   /**
    * Student part begin
    */
-
-  // calculate cross correlation matrix
-
-  // calculate Kalman gain K;
-
-  // update state mean and covariance matrix
-
+  // Calculate the cross-correlation matrix
+  // between the sigma points in the state- and measurement spaces
+  // Calculate the Kalman gain matrix `K`
+  // Update the state mean and covariance matrix
   /**
    * Student part end
    */
-
-  // print result
-  std::cout << "Updated state x: " << std::endl << x << std::endl;
-  std::cout << "Updated state covariance P: " << std::endl << P << std::endl;
-
-  // write result
+  // Print the resulting outputs
+  std::cout << "Updated state x: " << "\n" << x << "\n";
+  std::cout << "Updated state covariance P: " << "\n" << P << "\n";
+  // Update the input pointers to the outputs
   *x_out = x;
   *P_out = P;
 }
