b182f33c993ed1bde3bddd9c89d2ba0cfbdc9b97
jonathanloganmoran <jonathan.moran107@gmail.com>
2023-01-17 10:54:53 -0800

    :tada: Adding starter code for Extracurricular Exercise 4.4.1: Implement Naïve Bayes in C++

---
+++ 4-Planning/Exercises/4-4-Prediction/classifier.h
@@ -0,0 +1,38 @@
+#ifndef CLASSIFIER_H
+#define CLASSIFIER_H
+
+#include <string>
+#include <vector>
+#include "Dense"
+
+using Eigen::ArrayXd;
+using std::string;
+using std::vector;
+
+class GNB {
+ public:
+  /**
+   * Constructor
+   */
+  GNB();
+
+  /**
+   * Destructor
+   */
+  virtual ~GNB();
+
+  /**
+   * Train classifier
+   */
+  void train(const vector<vector<double>> &data, 
+             const vector<string> &labels);
+
+  /**
+   * Predict with trained classifier
+   */
+  string predict(const vector<double> &sample);
+
+  vector<string> possible_labels = {"left","keep","right"};
+};
+
+#endif  // CLASSIFIER_H
