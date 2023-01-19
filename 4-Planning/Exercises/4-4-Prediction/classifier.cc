6bced364f4ea0c57d8eeeb45175f9c540d02e354
jonathanloganmoran <jonathan.moran107@gmail.com>
2023-01-19 15:46:31 -0800

    :tada: Adding starter code for Extracurricular Exercise 4.4: Gaussian Naïve Bayes Classifier

--- 4-Planning/Exercises/4-4-Prediction/classifier.cc
+++ 4-Planning/Exercises/4-4-Prediction/classifier.cc
@@ -4,7 +4,8 @@
 #include <vector>
 
 using Eigen::ArrayXd;
-
+using std::string;
+using std::vector;
 
 // Initializes GNB
 GNB::GNB() {
@@ -13,37 +14,38 @@ GNB::GNB() {
    */
   
 }
+
 GNB::~GNB() {}
 
-
-/* Fits the Gaussian Naïve Bayes classifier on the provided data and labels. 
- *
- * @param  data     Nx4 array of observations, each assumed to be a tuple of
- *                  state values [`s`, `d`, `s_dot`, `d_dot`].
- * @param  labels   Nx1 array of labels, each label is a manoeuvre from one of
- *                  the possible strings: ["left", "keep", "right"].  
- */
-void GNB::train(
-    const std::vector<std::vector<double>>& data, 
-    const std::vector<std::string>& labels
-) {
+void GNB::train(const vector<vector<double>> &data, 
+                const vector<string> &labels) {
   /**
+   * Trains the classifier with N data points and labels.
+   * @param data - array of N observations
+   *   - Each observation is a tuple with 4 values: s, d, s_dot and d_dot.
+   *   - Example : [[3.5, 0.1, 5.9, -0.02],
+   *                [8.0, -0.3, 3.0, 2.2],
+   *                 ...
+   *                ]
+   * @param labels - array of N labels
+   *   - Each label is one of "left", "keep", or "right".
+   *
    * TODO: Implement the training function for your classifier.
    */
+  
 }
 
-
-/* Evaluates the give sample on the trained Gaussian Naïve Bayes classifier.
- *
- * @param    sample   1x4 tuple of state values in the form:
- *                    [`s`, `d`, `s_dot`, `d_dot`].
- * @returns  Label of the most-likely prediction for the given sample.
- */
-std::string GNB::predict(
-    const std::vector<double>& sample
-) {
+string GNB::predict(const vector<double> &sample) {
   /**
+   * Once trained, this method is called and expected to return 
+   *   a predicted behavior for the given observation.
+   * @param observation - a 4 tuple with s, d, s_dot, d_dot.
+   *   - Example: [3.5, 0.1, 8.5, -0.2]
+   * @output A label representing the best guess of the classifier. Can
+   *   be one of "left", "keep" or "right".
+   *
    * TODO: Complete this function to return your classifier's prediction
    */
-  return this->possible_labels[1];
+  
+  return this -> possible_labels[1];
 }
--- 4-Planning/Exercises/4-4-Prediction/classifier.h
+++ 4-Planning/Exercises/4-4-Prediction/classifier.h
@@ -1,46 +1,38 @@
 #ifndef CLASSIFIER_H
 #define CLASSIFIER_H
 
-#include "Dense"            // Eigen::ArrayXd
 #include <string>
 #include <vector>
+#include "Dense"
 
 using Eigen::ArrayXd;
+using std::string;
+using std::vector;
 
-
-/* The Gaussian Naïve Bayes classifier.
- *
- * Predicts the most-likely vehicle manoeuvre given a state vector of values:
- *    [`s`, `d`, `s_dot`, `d_dot`],
- * where each is double of position and velocity components defined in the
- * Frenet coordinate space.
- * 
- * The Guassian Naïve Bayes classifier exploits the Bayes rule assuming the
- * data is normally-distributed in order to predict the most-likely manoeuvre
- * from the set of three possible manoeuvres:
- *    ["left", "keep" "right"],
- * as defined in the `possible_labels` vector here.
- * 
- * @param  possible_labels  Set of possible vehicle manoeuvres to predict. 
- */
 class GNB {
  public:
+  /**
+   * Constructor
+   */
   GNB();
-  virtual ~GNB();
-  // Fits the Gaussian Naïve Bayes classifier on the provided data and labels
-  void train(
-      const std::vector<std::vector<double>>& data,
-      const std::vector<std::string>& labels
-  );
-  // Evaluates the give sample on the trained Gaussian Naïve Bayes classifier
-  string predict(const vector<double> &sample);
-  // Possible manoeuvres to predict with the GNBC
-  std::vector<std::string> possible_labels = {
-      "left",
-      "keep",
-      "right"
-  };
-};
 
+  /**
+   * Destructor
+   */
+  virtual ~GNB();
+
+  /**
+   * Train classifier
+   */
+  void train(const vector<vector<double>> &data, 
+             const vector<string> &labels);
+
+  /**
+   * Predict with trained classifier
+   */
+  string predict(const vector<double> &sample);
+
+  vector<string> possible_labels = {"left","keep","right"};
+};
 
 #endif  // CLASSIFIER_H
