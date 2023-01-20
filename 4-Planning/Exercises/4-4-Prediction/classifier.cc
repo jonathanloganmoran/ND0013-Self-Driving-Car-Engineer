8f3400d25c1d3cf4bdc7d5ffbaf1620069beb773
jonathanloganmoran <jonathan.moran107@gmail.com>
2023-01-19 16:05:58 -0800

    :rocket: Finishing Extracurricular Exercise 4.4: Finishing `GNB` class

--- 4-Planning/Exercises/4-4-Prediction/classifier.cc
+++ 4-Planning/Exercises/4-4-Prediction/classifier.cc
@@ -1,51 +1,215 @@
+/* ------------------------------------------------------------------------------
+ * Lesson "4.4: Prediction"
+ * Authors     : Mahni Shayganfar.
+ *
+ * Modified by : Jonathan L. Moran (jonathan.moran107@gmail.com)
+ *
+ * Purpose of this file: Implements the Gaussian Naïve Bayes (GNB) classifier.
+ * ----------------------------------------------------------------------------
+ */
+
 #include "classifier.h"
-#include <math.h>
-#include <string>
-#include <vector>
 
-using Eigen::ArrayXd;
-using std::string;
-using std::vector;
 
-// Initializes GNB
-GNB::GNB() {
-  /**
-   * TODO: Initialize GNB, if necessary. May depend on your implementation.
-   */
-  
+// Initialises GNB with ground-truth labels
+GNB::GNB()
+  : possible_labels({"left", "keep", "right"}) {
+  /*** Initialise the data tables ***/
+  for (const auto& label : this->possible_labels) {
+    this->class_priors.emplace(
+        std::make_pair<const std::string&, double>(
+            label, 0.0
+        )
+    );
+    this->data_table.emplace(
+        std::make_pair<const std::string&, std::vector<std::vector<double>>>(
+            label, std::vector<std::vector<double>>()
+        )
+    );
+    this->conditional_probabilities.emplace(
+        std::make_pair<const std::string&, std::vector<std::vector<double>>>(
+            label, std::vector<std::vector<double>>()
+        )
+    );
+  }
 }
-
+// Implements the destructor
 GNB::~GNB() {}
 
-void GNB::train(const vector<vector<double>> &data, 
-                const vector<string> &labels) {
-  /**
-   * Trains the classifier with N data points and labels.
-   * @param data - array of N observations
-   *   - Each observation is a tuple with 4 values: s, d, s_dot and d_dot.
-   *   - Example : [[3.5, 0.1, 5.9, -0.02],
-   *                [8.0, -0.3, 3.0, 2.2],
-   *                 ...
-   *                ]
-   * @param labels - array of N labels
-   *   - Each label is one of "left", "keep", or "right".
-   *
-   * TODO: Implement the training function for your classifier.
-   */
-  
+
+/* Computes the frequency of each class.
+ *
+ * The number of occurrences in `labels` of each class from `possible_labels`
+ * is counted and stored in a map instance such that the key is the class
+ * label and the value is the number of occurrences of the respective class
+ * in the given `labels` vector.
+ * 
+ * @param    labels         String-valued labels of every training example.
+ * @returns  class_counts   Frequency dict for each class in `possible_labels`.
+ */
+std::map<std::string, double> GNB::unique_occurrences(
+    const std::vector<std::string>& labels
+) {
+  // Initialise the map using the classes as the keys
+  std::map<std::string, double> class_counts;
+  for (const auto& label : this->possible_labels) {
+    // Initialise the count of the class label
+    // NOTE: `make_pair` is used to insert `const`-valued key
+    class_counts.emplace(
+        std::make_pair<const std::string&, double>(
+            label, 0.0
+        )
+    );
+  }
+  // Compute the frequency of each class in the training labels
+  for (const auto& label : labels) {
+    // Get the current class count
+    auto cls_iter = class_counts.find(label);
+    // Increment the class count (frequency)
+    cls_iter->second += 1;
+  }
+  return class_counts;
 }
 
-string GNB::predict(const vector<double> &sample) {
-  /**
-   * Once trained, this method is called and expected to return 
-   *   a predicted behavior for the given observation.
-   * @param observation - a 4 tuple with s, d, s_dot, d_dot.
-   *   - Example: [3.5, 0.1, 8.5, -0.2]
-   * @output A label representing the best guess of the classifier. Can
-   *   be one of "left", "keep" or "right".
-   *
-   * TODO: Complete this function to return your classifier's prediction
-   */
-  
-  return this -> possible_labels[1];
+
+/* Computes the per-class mean and standard deviation for each feature. 
+ *
+ * Tabularises the data for each class, then computes the per-class statistics
+ * for each feature. The resulting `feature_statistics` map contains Gaussian
+ * parameters `mu` and `sigma` computed w.r.t. each feature of each class in
+ * the training `data`.
+ * 
+ * @param  data     Training data, each sample is a vector of state values.
+ * @param  labels   Set of corresponding class labels for the training `data`.
+ */
+void GNB::compute_statistics(
+    const std::vector<std::vector<double>>& data,
+    const std::vector<std::string>& labels
+) {
+  /*** Construct the tabular dataset ***/
+  // Initialise vector of Gaussian distribution parameters
+  std::vector<Stats> mu_sigma;
+  if (labels.size() != data.size()) {
+    throw "ERROR: Labels and data are of different lengths";
+  }
+  for (int i = 0; i < data[0].size(); i++) {
+    // Initialise each feature in feature vector with zero distribution values
+    mu_sigma.push_back(Stats{0.0, 0.0});
+  }
+  for (const auto& label : this->possible_labels) {
+    // Initialise each key in the data table
+    this->feature_statistics.emplace(
+        std::make_pair<const std::string&, std::vector<Stats>>(
+            label, std::vector<Stats>{mu_sigma}
+        )
+    );
+  }
+  // Compute the class frequencies
+  std::map<std::string, double> freq = unique_occurrences(labels);
+  /*** Compute the per-class mean of each feature ***/
+  for (int i = 0; i < labels.size(); i++) {
+    data_table[labels[i]].push_back(data[i]);
+    for (int idx_feat = 0; idx_feat < data[i].size(); idx_feat++) {
+      this->feature_statistics[labels[i]][idx_feat].mu += data[i][idx_feat];
+    }
+  }
+  for (const auto& label : this->possible_labels) {
+    for (int idx_feat = 0; idx_feat < data[0].size(); idx_feat++) {
+      this->feature_statistics[label][idx_feat].mu /= freq[label];
+    }
+  }
+  /*** Compute the per-class standard deviation of each feature ***/
+  for (const auto& label : this->possible_labels) {
+    for (const auto& cls_data : data_table[label]) {
+      for (int idx_feat = 0; idx_feat < cls_data.size(); idx_feat++) {
+        this->feature_statistics[label][idx_feat].sigma += std::pow(
+            cls_data[idx_feat] - this->feature_statistics[label][idx_feat].mu,
+            2
+        );
+      }
+    }
+  }
+  for (const auto& label : this->possible_labels) {
+    for (int idx_feat = 0; idx_feat < data[0].size(); idx_feat++) {
+      this->feature_statistics[label][idx_feat].sigma /= freq[label];
+      this->feature_statistics[label][idx_feat].sigma = std::sqrt(
+          this->feature_statistics[label][idx_feat].sigma
+      );
+    }
+  }
+  // Update the class priors
+  for (const auto& label : this->possible_labels) {
+    // Compute the current class prior probability
+   class_priors[label] = freq[label] / labels.size();
+  }
+}
+
+
+/* Evaluates the Gaussian probability density function for the given values.
+ *
+ * @param   x               Current feature value to evaluate.
+ * @param   mu              Mean value of the feature / label combination.
+ * @param   sigma           Standard deviation of the feature / label combination.
+ * @returns p_conditional   Resulting conditional probability value.
+ */
+double GNB::gaussian_pdf(
+    double x,
+    double mu,
+    double sigma
+) {
+  double sigma_squared = sigma * sigma;
+  double p_conditional = 1.0 / std::sqrt(2 * M_PI * sigma_squared);
+  p_conditional *= std::exp(
+      -std::pow((x - mu), 2) / (2.0 * sigma_squared)
+  );
+  return p_conditional;
+}
+
+
+/* Fits the Gaussian Naïve Bayes classifier on the provided data and labels. 
+ *
+ * @param  data     Nx4 array of observations, each assumed to be a tuple of
+ *                  state values [`s`, `d`, `s_dot`, `d_dot`].
+ * @param  labels   Nx1 array of labels, each label is a manoeuvre from one of
+ *                  the possible strings: ["left", "keep", "right"].  
+ */
+void GNB::train(
+    const std::vector<std::vector<double>>& data, 
+    const std::vector<std::string>& labels
+) {
+  /*** Fit the per-class Gaussian distribution for each feature ***/
+  compute_statistics(data, labels);
+}
+
+
+/* Evaluates the given sample on the trained Gaussian Naïve Bayes classifier.
+ *
+ * @param    sample   1x4 tuple of state values in the form:
+ *                    [`s`, `d`, `s_dot`, `d_dot`].
+ * @returns  Label of the most-likely prediction for the given sample.
+ */
+std::string GNB::predict(
+    const std::vector<double>& sample
+) {
+  std::map<std::string, double> y_pred;
+  for (const auto& label : this->possible_labels) {
+    double p_posterior = this->class_priors[label]; 
+    // Compute the per-class conditional probabilities
+    for (int idx_feat = 0; idx_feat < sample.size(); idx_feat++) {
+      double p_cond = gaussian_pdf(
+          sample[idx_feat],
+          this->feature_statistics[label][idx_feat].mu,
+          this->feature_statistics[label][idx_feat].sigma
+      );
+      p_posterior *= p_cond;
+    }
+    y_pred[label] = p_posterior;
+  }
+  // Compute the most-likely class (i.e., the argmax)
+  return std::max_element(
+      y_pred.begin(),
+      y_pred.end(),
+      [](const std::pair<std::string, double>& a, const std::pair<std::string, double> b) {
+          return a.second < b.second; 
+      })->first;
 }
--- 4-Planning/Exercises/4-4-Prediction/classifier.h
+++ 4-Planning/Exercises/4-4-Prediction/classifier.h
@@ -1,38 +1,88 @@
+/* ------------------------------------------------------------------------------
+ * Lesson "4.4: Prediction"
+ * Authors     : Mahni Shayganfar.
+ *
+ * Modified by : Jonathan L. Moran (jonathan.moran107@gmail.com)
+ *
+ * Purpose of this file: Header file for the Gaussian Naïve Bayes classifier.
+ * ----------------------------------------------------------------------------
+ */
+
 #ifndef CLASSIFIER_H
 #define CLASSIFIER_H
 
 #include <string>
 #include <vector>
-#include "Dense"
+#include <algorithm>
+#include <cmath>
+#include <map>
+#include <utility>
 
-using Eigen::ArrayXd;
-using std::string;
-using std::vector;
 
+/* The Gaussian Naïve Bayes classifier.
+ *
+ * Predicts the most-likely vehicle manoeuvre given a state vector of values:
+ *    [`s`, `d`, `s_dot`, `d_dot`],
+ * where each is double of position and velocity components defined in the
+ * Frenet coordinate space.
+ * 
+ * The Guassian Naïve Bayes classifier exploits the Bayes rule assuming the
+ * data in each class (the state vector values) is normally-distributed to
+ * predict the most-likely manoeuvre from the set of three possible manoeuvres:
+ *    ["left", "keep" "right"],
+ * as defined in the `possible_labels` vector here.
+ * 
+ * @param  possible_labels  Set of possible vehicle manoeuvres to predict. 
+ */
 class GNB {
  public:
-  /**
-   * Constructor
-   */
   GNB();
-
-  /**
-   * Destructor
-   */
   virtual ~GNB();
-
-  /**
-   * Train classifier
-   */
-  void train(const vector<vector<double>> &data, 
-             const vector<string> &labels);
-
-  /**
-   * Predict with trained classifier
-   */
-  string predict(const vector<double> &sample);
-
-  vector<string> possible_labels = {"left","keep","right"};
+  // Fits the Gaussian Naïve Bayes classifier on the provided data and labels
+  void train(
+      const std::vector<std::vector<double>>& data,
+      const std::vector<std::string>& labels
+  );
+  // Evaluates the give sample on the trained Gaussian Naïve Bayes classifier
+  std::string predict(
+      const std::vector<double>& sample
+  );
+  // Computes the number of occurrences of the class labels in the vector
+  std::map<std::string, double> unique_occurrences(
+      const std::vector<std::string>& labels
+  );
+  // Computes the Gaussian distribution statistics
+  void compute_statistics(
+      const std::vector<std::vector<double>>& data,
+      const std::vector<std::string>& labels
+  );
+  // Evaluate the Gaussian probability density function
+  double gaussian_pdf(
+    double x,
+    double mu,
+    double sigma
+  );
+  // Stores the distribution parameters for each feature
+  struct Stats {
+    // Mean value
+    double mu;
+    // Standard deviation
+    double sigma;
+  };
+  // Possible manoeuvres to predict with the GNBC
+  // NOTE: should initialise with member initialisation list
+  std::vector<std::string> possible_labels;
+  /*** Data tables ***/
+  // NOTE: should initialise these inside constructor { body }
+  // Per-class prior probabilities
+  std::map<std::string, double> class_priors;
+  // Data table to store per-class examples
+  std::map<std::string, std::vector<std::vector<double>>> data_table;
+  // Conditional probabilities for all labels w.r.t. the feature values
+  std::map<std::string, std::vector<std::vector<double>>> conditional_probabilities;
+  // Distribution parameters for each feature
+  std::map<std::string, std::vector<Stats>> feature_statistics;
 };
+
 
 #endif  // CLASSIFIER_H
