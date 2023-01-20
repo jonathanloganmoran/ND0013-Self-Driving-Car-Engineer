70a172058cbf82df2375ca2f75ee2cc73ef40627
jonathanloganmoran <jonathan.moran107@gmail.com>
2023-01-20 10:02:50 -0800

    :tada: Adding starter code for Extracurricular Exercise 5.2.1: Global Kinematic Model

---
+++ 5-Control/Exercises/5-2-Vehicle-Models/1_global_kinematic_model.cc
@@ -0,0 +1,49 @@
+// In this quiz you'll implement the global kinematic model.
+#include <math.h>
+#include <iostream>
+#include "Dense"
+
+using Eigen::VectorXd;
+
+//
+// Helper functions
+//
+constexpr double pi() { return M_PI; }
+double deg2rad(double x) { return x * pi() / 180; }
+double rad2deg(double x) { return x * 180 / pi(); }
+
+const double Lf = 2;
+
+// Return the next state.
+VectorXd globalKinematic(const VectorXd &state, 
+                         const VectorXd &actuators, double dt);
+
+int main() {
+  // [x, y, psi, v]
+  VectorXd state(4);
+  // [delta, v]
+  VectorXd actuators(2);
+
+  state << 0, 0, deg2rad(45), 1;
+  actuators << deg2rad(5), 1;
+
+  // should be [0.212132, 0.212132, 0.798488, 1.3]
+  auto next_state = globalKinematic(state, actuators, 0.3);
+
+  std::cout << next_state << std::endl;
+}
+
+VectorXd globalKinematic(const VectorXd &state, 
+                         const VectorXd &actuators, double dt) {
+  // Create a new vector for the next state.
+  VectorXd next_state(state.size());
+
+  /**
+   * TODO: Implement the global kinematic model,
+   *   to return the next state from the inputs.
+   */
+
+  // NOTE: state is [x, y, psi, v] and actuators is [delta, a]
+
+  return next_state;
+}
