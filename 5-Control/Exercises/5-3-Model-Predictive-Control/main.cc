12b7f7a37d60419ae145f1a50fa6998c983983c5
jonathanloganmoran <jonathan.moran107@gmail.com>
2023-01-21 12:41:42 -0800

    :art::bulb: Adding docstrings and enforcing style Added here are detailed docstrings covering the implementation of the MPC controller as well as the use of Matplotlib C++ API to plot the controller error and vehicle actuator values. Also included in this commit is the removal of several STL (`std::vector` and `std::cout`) and Eigen (`Eigen::VectorXd`) namespaces. Preserved is the use of `plt = matplotlibcpp` for readability for those who are familiar with the Matplotlib Python API.

--- 5-Control/Exercises/5-3-Model-Predictive-Control/main.cc
+++ 5-Control/Exercises/5-3-Model-Predictive-Control/main.cc
@@ -1,31 +1,60 @@
-#include <vector>
-#include "Eigen-3.3/Eigen/QR"
+/* ----------------------------------------------------------------------------
+ * Lesson "5.3: Model Predictive Control"
+ * Authors     : David Siller, Andrew Gray, Dominique Luna.
+ *
+ * Modified by : Jonathan L. Moran (jonathan.moran107@gmail.com) 
+ *
+ * Purpose of this file: Performs lane-keeping with Model Predictive Control.
+ * ----------------------------------------------------------------------------
+ */
+
+#include "mpc.h"
 #include "helpers.h"
 #include "matplotlibcpp.h"
-#include "MPC.h"
+#include "Eigen-3.3/Eigen/QR"
+#include <vector>
 
 namespace plt = matplotlibcpp;
 
-using Eigen::VectorXd;
-using std::cout;
-using std::endl;
-using std::vector;
 
+/* Runs the vehicle simulation.
+ *
+ * The vehicle "Follow Lane" manoeuvre is performed using the Model Predictive 
+ * Control (MPC)-based controller. The MPC consists of solving a non-linear
+ * optimisation problem in order to compute the next-state of the vehicle over
+ * a pre-defined time horizon of `N` steps. The desired lane-keeping manoeuvre
+ * is performed by iteratively computing the next-best lowest-cost state of the
+ * vehicle. This is done by solving the non-linear function formed by the
+ * polynomial using the IPOPT algorithm. The MPC iterates over the trajectory
+ * waypoints and simulates the vehicle state over `N` number of time-steps.
+ * A set of actuator commands are realised ("performed") for each simulation
+ * step and their associated cost is computed and minimised w.r.t. the three
+ * cost functions defined in `MPC`:
+ *    1. Deviation from reference trajectory;
+ *    2. Comfort constraints;
+ *    3. Efficiency constraints. 
+ * These three cost functions are used to penalise the vehicle behaviour and
+ * guide the optimisation problem towards a cost-minimal solution, i.e., the
+ * most-desirable next-state for the vehicle based on a realised time-horizon.
+ * Once the cost-minimal next-state of the vehicle has been selected, the
+ * remaining waypoints of the trajectory are discarded; a new trajectory
+ * estimate is then formed and the cost-minimisation problem is repeated.
+ */
 int main() {
   MPC mpc;
+  // Set the number of simulation iterations to perform
+  // CANDO: modify this value
   int iters = 50;
-
-  VectorXd ptsx(2);
-  VectorXd ptsy(2);
+  Eigen::VectorXd ptsx(2);
+  Eigen::VectorXd ptsy(2);
   ptsx << -100, 100;
   ptsy << -1, -1;
-
   /**
    * TODO: fit a polynomial to the above x and y coordinates
    */
   auto coeffs = ? ;
-
-  // NOTE: free feel to play around with these
+  // Set the initial state values
+  // CANDO: modify these values
   double x = -1;
   double y = 10;
   double psi = 0;
@@ -38,49 +67,48 @@ int main() {
    * TODO: calculate the orientation error
    */
   double epsi = ? ;
-
-  VectorXd state(6);
+  Eigen::VectorXd state(6);
+  // Read in the initial state vector values
   state << x, y, psi, v, cte, epsi;
-
-  vector<double> x_vals = {state[0]};
-  vector<double> y_vals = {state[1]};
-  vector<double> psi_vals = {state[2]};
-  vector<double> v_vals = {state[3]};
-  vector<double> cte_vals = {state[4]};
-  vector<double> epsi_vals = {state[5]};
-  vector<double> delta_vals = {};
-  vector<double> a_vals = {};
-
+  // Add the initial state values to the state history vectors
+  std::vector<double> x_vals = {state[0]};
+  std::vector<double> y_vals = {state[1]};
+  std::vector<double> psi_vals = {state[2]};
+  std::vector<double> v_vals = {state[3]};
+  std::vector<double> cte_vals = {state[4]};
+  std::vector<double> epsi_vals = {state[5]};
+  // Add the initial actuator values to the actuator history vectors
+  std::vector<double> delta_vals = {};
+  std::vector<double> a_vals = {};
+  // Perform the simulation iterations
   for (size_t i = 0; i < iters; ++i) {
-    cout << "Iteration " << i << endl;
-
+    std::cout << "Iteration " << i << "\n";
+    // Solve the non-linear optimisation problem with IPOPT
     auto vars = mpc.Solve(state, coeffs);
-
+    // Append the next-state values to the state history vectors
     x_vals.push_back(vars[0]);
     y_vals.push_back(vars[1]);
     psi_vals.push_back(vars[2]);
     v_vals.push_back(vars[3]);
     cte_vals.push_back(vars[4]);
     epsi_vals.push_back(vars[5]);
-
+    // Append the next-state actuator values to the actuator history vectors
     delta_vals.push_back(vars[6]);
     a_vals.push_back(vars[7]);
-
+    // Update the current state with the next-state values
     state << vars[0], vars[1], vars[2], vars[3], vars[4], vars[5];
-    cout << "x = " << vars[0] << endl;
-    cout << "y = " << vars[1] << endl;
-    cout << "psi = " << vars[2] << endl;
-    cout << "v = " << vars[3] << endl;
-    cout << "cte = " << vars[4] << endl;
-    cout << "epsi = " << vars[5] << endl;
-    cout << "delta = " << vars[6] << endl;
-    cout << "a = " << vars[7] << endl;
-    cout << endl;
+    std::cout << "x = " << vars[0] << "\n";
+    std::cout << "y = " << vars[1] << "\n";
+    std::cout << "psi = " << vars[2] << "\n";
+    std::cout << "v = " << vars[3] << "\n";
+    std::cout << "cte = " << vars[4] << "\n";
+    std::cout << "epsi = " << vars[5] << "\n";
+    std::cout << "delta = " << vars[6] << "\n";
+    std::cout << "a = " << vars[7] << "\n";
+    std::cout << "\n";
   }
-
-  // Plot values
-  // NOTE: feel free to play around with this.
-  // It's useful for debugging!
+  /*** Plotting the actuator and error values with Matplotlib ***/
+  // CANDO: modify the plotting for debugging purposes
   plt::subplot(3, 1, 1);
   plt::title("CTE");
   plt::plot(cte_vals);
@@ -90,6 +118,5 @@ int main() {
   plt::subplot(3, 1, 3);
   plt::title("Velocity");
   plt::plot(v_vals);
-
   plt::show();
 }
