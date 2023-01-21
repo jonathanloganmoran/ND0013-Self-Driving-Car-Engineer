ae2df759d67a23a4630310a8615ee14316d1cdd7
jonathanloganmoran <jonathan.moran107@gmail.com>
2023-01-20 17:25:33 -0800

    :tada: Adding starter code for Extracurricular Exercise 5.3.1: Model Predictive Control

---
+++ 5-Control/Exercises/5-3-Model-Predictive-Control/mpc.h
@@ -0,0 +1,20 @@
+#ifndef MPC_H
+#define MPC_H
+
+#include <vector>
+#include "Eigen-3.3/Eigen/Core"
+
+
+class MPC {
+ public:
+  MPC();
+
+  virtual ~MPC();
+
+  // Solve the model given an initial state.
+  // Return the next state and actuations as a vector.
+  std::vector<double> Solve(const Eigen::VectorXd &x0, 
+                            const Eigen::VectorXd &coeffs);
+};
+
+#endif  // MPC_H
