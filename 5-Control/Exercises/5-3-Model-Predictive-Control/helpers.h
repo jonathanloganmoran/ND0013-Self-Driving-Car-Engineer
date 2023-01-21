01a60ff985a5b27aa5bcc58c9c6a73d188ba29cd
jonathanloganmoran <jonathan.moran107@gmail.com>
2023-01-20 17:26:51 -0800

    :sparkles: Adding `helpers` (polynomial fitting from EE5.2.1)

---
+++ 5-Control/Exercises/5-3-Model-Predictive-Control/helpers.h
@@ -0,0 +1,48 @@
+/* Evaluates the `y` coordinates of a given polynomial.
+ *
+ * @param    coeffs   Coefficients of the fitted polynomial.
+ * @param    x        Coordinates of the waypoints along the $x$-axis.
+ * @returns  result   The corresponding coordinates along the $y$-axis.
+ */
+double polyeval(
+    const Eigen::VectorXd& coeffs, 
+    double x
+) {
+  double result = 0.0;
+  for (int i = 0; i < coeffs.size(); ++i) {
+    result += coeffs[i] * pow(x, i);
+  }
+  return result;
+}
+
+
+/* Fits a polynomial curve to the given waypoints.
+ *
+ * Adapted from:
+ * https://github.com/JuliaMath/Polynomials.jl/blob/master/src/Polynomials.jl#L676-L716
+ * 
+ * @param    yvals    Coordinates of the waypoints along the $y$-axis.
+ * @param    xvals    Coordinates of the waypoints along the $x$-axis.
+ * @param    order    Order of the polynomial to fit.
+ * @returns  result   The vector of coefficients of the fitted polynomial.
+ */
+Eigen::VectorXd polyfit(
+    const Eigen::VectorXd& xvals, 
+    const Eigen::VectorXd& yvals, 
+    int order
+) {
+  assert(xvals.size() == yvals.size());
+  assert(order >= 1 && order <= xvals.size() - 1);
+  Eigen::MatrixXd A(xvals.size(), order + 1);
+  for (int i = 0; i < xvals.size(); ++i) {
+    A(i, 0) = 1.0;
+  }
+  for (int j = 0; j < xvals.size(); ++j) {
+    for (int i = 0; i < order; ++i) {
+      A(j, i + 1) = A(j, i) * xvals(j);
+    }
+  }
+  auto Q = A.householderQr();
+  Eigen::VectorXd result = Q.solve(yvals);
+  return result;
+}
