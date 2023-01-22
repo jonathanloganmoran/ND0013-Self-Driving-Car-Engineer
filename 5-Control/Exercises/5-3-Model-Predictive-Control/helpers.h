b0119ec086f2fcb4086ab989001c6332c381c5b3
jonathanloganmoran <jonathan.moran107@gmail.com>
2023-01-22 13:13:41 -0800

    :rotating_light: Fixing warnings — using `std::` namespace

--- 5-Control/Exercises/5-3-Model-Predictive-Control/helpers.h
+++ 5-Control/Exercises/5-3-Model-Predictive-Control/helpers.h
@@ -26,7 +26,7 @@ double polyeval(
 ) {
   double result = 0.0;
   for (int i = 0; i < coeffs.size(); ++i) {
-    result += coeffs[i] * pow(x, i);
+    result += coeffs[i] * std::pow(x, i);
   }
   return result;
 }
